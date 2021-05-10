99ba7f675b755119d4b636a86ddfceadc3996ff5
Galen <363693158@qq.com>
2021-05-10 11:53:58 +0800

    chore: use GA for tracking (#8)

--- src/ui/component/ArrowLink.tsx
+++ src/ui/component/ArrowLink.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, LinkProps } from 'react-router-dom';
 import cx from 'clsx';
 import { Icon } from 'ui/component';
 
-const ArrowLink = ({ children, className, ...restProps }) => (
-  <Link className={cx('w-full flex items-center', className)} {...restProps}>
+const ArrowLink = ({ children, className, to, ...restProps }: LinkProps) => (
+  <Link
+    className={cx('w-full flex items-center', className)}
+    to={to}
+    {...restProps}
+  >
     <div className="flex-1">{children}</div>
     <Icon type="arrow" />
   </Link>
