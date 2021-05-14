29ac5dd145946e0ee57c8840d346658ec638fa9d
Galen <363693158@qq.com>
2021-05-14 12:08:26 +0800

    feat: + field component (#14)

---
+++ src/ui/component/Field/index.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode } from 'react';
+import './style.less';
+
+interface FieldProps {
+  children: ReactNode;
+  leftIcon: ReactNode;
+  rightIcon: ReactNode;
+  onClick?(): void;
+}
+
+const Field = ({ children, leftIcon, rightIcon, onClick }: FieldProps) => {
+  return (
+    <div
+      className="field"
+      onClick={onClick}
+      style={{ cursor: onClick ? 'pointer' : 'initial' }}
+    >
+      <div className="left-icon">{leftIcon}</div>
+      <div className="field-slot">{children}</div>
+      <div className="right-icon">{rightIcon}</div>
+    </div>
+  );
+};
+
+export default Field;
