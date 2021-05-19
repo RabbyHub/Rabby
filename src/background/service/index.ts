d6f4d189278555f126193390176cc2d0ccd4216c
Galen <363693158@qq.com>
2021-05-19 17:51:39 +0800

    feat: + openapi management (#22)

--- src/background/service/index.ts
+++ src/background/service/index.ts
@@ -4,3 +4,4 @@ export { default as permission } from './permission';
 export { default as preference } from './preference';
 export { default as session } from './session';
 export { default as chainService } from './chain';
+export { default as openapi } from './openapi';
