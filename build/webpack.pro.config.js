2b65b7c4c80262f3b9e27fd414dc1dc17d8d1360
see2ever <see2ever@gmail.com>
2021-05-28 11:53:42 +0800

    fbuild: remove BundleAnalyzerPlugin in pro

--- build/webpack.pro.config.js
+++ build/webpack.pro.config.js
@@ -1,11 +1,11 @@
 const webpack = require('webpack');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const config = {
   mode: 'production',
   devtool: false,
   plugins: [
-    new BundleAnalyzerPlugin(),
+    // new BundleAnalyzerPlugin(),
     new webpack.DefinePlugin({
       'process.env.BUILD_ENV': JSON.stringify('PRO'),
     })
