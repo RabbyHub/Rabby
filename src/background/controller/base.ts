3e5adbbc7a9d0d2299a437a7ef24d0667cc19187
Galen <363693158@qq.com>
2021-06-02 19:27:27 +0800

    feat: make get current account sync (#62)

--- src/background/controller/base.ts
+++ src/background/controller/base.ts
@@ -29,6 +29,10 @@ class BaseController {
     };
   };
 
+  syncGetCurrentAccount = () => {
+    return preferenceService.getCurrentAccount() || null;
+  };
+
   getAccounts = () => {
     return keyringService.getAllVisibleAccountsArray();
   };
