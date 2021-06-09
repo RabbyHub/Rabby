6a971eb088d0bbaa2eba12c646532149ae6d7aa8
Galen <363693158@qq.com>
2021-06-09 17:50:03 +0800

    feat: new sign text approval

--- src/ui/views/Approval/components/WatchAdrressWaiting.tsx
+++ src/ui/views/Approval/components/WatchAdrressWaiting.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button } from 'antd';
 import QRCode from 'qrcode.react';
 import { useApproval } from 'ui/utils';
+import AccountCard from './AccountCard';
 import { QrScan } from 'ui/assets';
 
 const WatchAddressWaiting = ({
@@ -19,28 +20,31 @@ const WatchAddressWaiting = ({
   };
 
   return (
-    <div className="hardware-operation">
-      <img src={QrScan} className="mb-[27px]" />
-      {/* <QRCode value={address} size={212} /> */}
-      <h1 className="text-15 text-medium text-gray-title text-center mb-[40px] px-[40px]">
-        Cannot sign using a watch mode address at this moment
-      </h1>
-      <p className="text-13 text-medium text-gray-content text-center">
-        Sign via phone scanning coming soon
-      </p>
-      <footer>
-        <div className="action-buttons flex justify-center">
-          <Button
-            type="primary"
-            size="large"
-            className="w-[172px]"
-            onClick={handleCancel}
-          >
-            Cancel
-          </Button>
-        </div>
-      </footer>
-    </div>
+    <>
+      <AccountCard />
+      <div className="watch-operation">
+        <img src={QrScan} className="mb-[27px]" />
+        {/* <QRCode value={address} size={212} /> */}
+        <h1 className="text-15 text-medium text-gray-title text-center mb-[40px] px-[40px]">
+          Cannot sign using a watch mode address at this moment
+        </h1>
+        <p className="text-13 text-medium text-gray-content text-center">
+          Sign via phone scanning coming soon
+        </p>
+        <footer>
+          <div className="action-buttons flex justify-center">
+            <Button
+              type="primary"
+              size="large"
+              className="w-[172px]"
+              onClick={handleCancel}
+            >
+              Cancel
+            </Button>
+          </div>
+        </footer>
+      </div>
+    </>
   );
 };
 
