d3c8252690d55012bb045e74338cfdbfa499e3d1
Galen <363693158@qq.com>
2021-06-09 16:17:36 +0800

    feat: new connect and chain approval

--- src/ui/views/Approval/components/AddChain.tsx
+++ src/ui/views/Approval/components/AddChain.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Button, Modal } from 'antd';
+import clsx from 'clsx';
+import { Button } from 'antd';
 import { CHAINS } from 'consts';
 import { useWallet, useApproval } from 'ui/utils';
+import IconWarning from 'ui/assets/warning.svg';
 
 interface AddChainProps {
   data: {
@@ -26,70 +28,90 @@ const AddChain = ({ params }: { params: AddChainProps }) => {
   const supportChains = wallet.getSupportChains();
   const showChain = supportChains.find((chain) => chain.hex === chainId);
 
-  if (!showChain) {
-    Modal.error({
-      title: 'Error',
-      content: 'The requested chain is not supported by Rabby yet.',
-      onOk: () => rejectApproval(),
-    });
-    return;
-  }
-
   const enableChains = wallet.getEnableChains();
   const { chain: defaultChain } = wallet.getConnectedSite(session.origin)!;
 
   let title;
   let content;
   if (!enableChains.some((chain) => chain.hex === chainId)) {
-    title = 'Wants to Enable a Chain';
-    content = 'By enabling a chain, you’ll be able to make transactions on it';
+    title = 'Enable a Chain';
+    content = 'By enabling a chain, you’ll be able to make transactions on it.';
   } else if (CHAINS[defaultChain].hex !== chainId) {
-    title = 'Wants to Switch to the Chain';
-    content =
-      'The next time you visit, this site will be connected to Ethereum';
+    title = 'Switch a Chain';
+    content = 'By enabling a chain, you’ll be able to make transactions on it.';
   }
 
   return (
     <>
-      <div className="approval-text">
-        <div className="site-card">
-          <img className="icon icon-site" src={session.icon} />
-          <div className="site-info">
-            <p className="font-medium text-gray-subTitle mb-0 text-13">
-              {session.origin}
-            </p>
-            <p className="text-12 text-gray-content mb-0">{session.name}</p>
-          </div>
-        </div>
-        <h1 className="text-center mb-24">{title}</h1>
-        <div className="text-center">
-          <img className="w-[64px] h-[64px] mx-auto" src={showChain.logo} />
-          <div className="mb-8 text-20 text-gray-title">{showChain.name}</div>
-          <div className="mb-24 text-14 text-gray-content">
-            Chain ID: {showChain.id}
-          </div>
-        </div>
-        <div className="text-center text-14 text-gray-content">{content}</div>
+      <div className="approval-chain">
+        {showChain ? (
+          <>
+            <h1 className="text-center mb-24">{title}</h1>
+            <div className="text-center">
+              <img
+                className="w-[44px] h-[44px] mx-auto mb-12"
+                src={showChain.logo}
+              />
+              <div className="mb-4 text-20 text-gray-title">
+                {showChain.name}
+              </div>
+              <div className="mb-28 text-14 text-gray-content">
+                Chain ID:{showChain.id}
+              </div>
+            </div>
+            <div className="text-center text-13 text-gray-content font-medium">
+              {content}
+            </div>
+          </>
+        ) : (
+          <>
+            <img
+              src={IconWarning}
+              className="w-[68px] h-[68px] mb-28 mx-auto"
+            />
+            <div className="text-gray-title text-20 w-[344px] mx-auto font-medium text-center">
+              The requested chain is not supported by Rabby yet
+            </div>
+          </>
+        )}
       </div>
       <footer className="connect-footer">
-        <div className="risk-info"></div>
-        <div className="action-buttons flex justify-between">
-          <Button
-            type="primary"
-            size="large"
-            className="w-[172px]"
-            onClick={rejectApproval}
-          >
-            Cancel
-          </Button>
-          <Button
-            type="primary"
-            size="large"
-            className="w-[172px]"
-            onClick={resolveApproval}
-          >
-            Change
-          </Button>
+        <div
+          className={clsx([
+            'action-buttons',
+            'flex',
+            showChain ? 'justify-between' : 'justify-center',
+          ])}
+        >
+          {showChain ? (
+            <>
+              <Button
+                type="primary"
+                size="large"
+                className="w-[172px]"
+                onClick={rejectApproval}
+              >
+                Cancel
+              </Button>
+              <Button
+                type="primary"
+                size="large"
+                className="w-[172px]"
+                onClick={resolveApproval}
+              >
+                Allow
+              </Button>
+            </>
+          ) : (
+            <Button
+              type="primary"
+              size="large"
+              className="w-[200px]"
+              onClick={rejectApproval}
+            >
+              OK
+            </Button>
+          )}
         </div>
       </footer>
     </>
