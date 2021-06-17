74c157b59bd56eb9882dc013f3a53fe689d4c245
Galen <363693158@qq.com>
2021-06-17 14:49:08 +0800

    fix: use track to fix chrome render problem in mac external screen

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -5,6 +5,40 @@ import Views from './views';
 
 import './style/index.less';
 
+// For fix chrome extension render problem in external screen
+if (
+  // From testing the following conditions seem to indicate that the popup was opened on a secondary monitor
+  window.screenLeft < 0 ||
+  window.screenTop < 0 ||
+  window.screenLeft > window.screen.width ||
+  window.screenTop > window.screen.height
+) {
+  chrome.runtime.getPlatformInfo(function (info) {
+    if (info.os === 'mac') {
+      const fontFaceSheet = new CSSStyleSheet();
+      fontFaceSheet.insertRule(`
+        @keyframes redraw {
+          0% {
+            opacity: 1;
+          }
+          100% {
+            opacity: .99;
+          }
+        }
+      `);
+      fontFaceSheet.insertRule(`
+        html {
+          animation: redraw 1s linear infinite;
+        }
+      `);
+      (document as any).adoptedStyleSheets = [
+        ...(document as any).adoptedStyleSheets,
+        fontFaceSheet,
+      ];
+    }
+  });
+}
+
 browser.runtime.getBackgroundPage().then((win) => {
   ReactDOM.render(
     <Views wallet={win.wallet} />,
