67c47bdbc51a84bdc19e05abdd3bbeeadc47c4a6
see2ever <see2ever@gmail.com>
2021-06-23 17:12:14 +0800

    feat(service): add disconnect broadcast when delete connect sites

--- src/content-script/pageProvider/index.ts
+++ src/content-script/pageProvider/index.ts
@@ -53,8 +53,12 @@ export class EthereumProvider extends EventEmitter {
   }
 
   initialize = async () => {
-    this._bcm.connect().on('message', this._handleBackgroundMessage);
+    document.addEventListener(
+      'visibilitychange',
+      this._requestPromiseCheckVisibility
+    );
 
+    this._bcm.connect().on('message', this._handleBackgroundMessage);
     domReadyCall(() => {
       const origin = top.location.origin;
       const icon =
@@ -94,11 +98,6 @@ export class EthereumProvider extends EventEmitter {
       this._initialized = true;
       this.emit('_initialized');
     }
-
-    document.addEventListener(
-      'visibilitychange',
-      this._requestPromiseCheckVisibility
-    );
   };
 
   private _requestPromiseCheckVisibility = () => {
