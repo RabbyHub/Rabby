8782e71bd14ba98a32dcb471193f9f8b4811e12e
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2021-07-02 10:47:11 +0800

    chore: + bundle script

--- build/release.js
+++ build/release.js
@@ -2,6 +2,9 @@ const path = require('path');
 const { prompt } = require('enquirer');
 const fs = require('fs-extra');
 const shell = require('shelljs');
+const zipdir = require('zip-dir');
+
+const PROJECT_ROOT = path.resolve(__dirname, '..');
 
 async function release() {
   const input = await prompt({
@@ -9,7 +12,7 @@ async function release() {
     name: 'version',
     message: '[Rabby] Please input the release version:'
   });
-  const manifestPath = path.resolve(__dirname, '..', '_raw', 'manifest.json');
+  const manifestPath = path.resolve(PROJECT_ROOT, '_raw', 'manifest.json');
   const manifest = fs.readJSONSync(manifestPath);
   manifest.version = input.version;
   fs.writeJSONSync(manifestPath, manifest, { spaces: 2 });
@@ -18,6 +21,14 @@ async function release() {
   shell.exec(`git commit -m "[release] ${input.version}"`);
   shell.exec(`git push origin refs/tags/v${input.version}`);
   shell.exec('git push origin master');
+
+  return input.version
 }
 
-release();
+function bundle(version) {
+  shell.exec('yarn build:pro');
+  const distPath = path.resolve(PROJECT_ROOT, 'dist');
+  zipdir(distPath, { saveTo: `Rabby_v${version}.zip` });
+}
+
+release().then(bundle);
