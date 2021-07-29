6add3348e9142b23966a85e8ad691e26e4af946f
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2021-07-29 12:20:29 +0800

    feat: + tooltip when disable

--- src/ui/views/ImportMode.tsx
+++ src/ui/views/ImportMode.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import { Tooltip } from 'antd';
 import { StrayPageWithButton, FieldCheckbox } from 'ui/component';
 import { useWallet } from 'ui/utils';
 import { KEYRING_CLASS } from 'consts';
@@ -12,7 +13,7 @@ const ImportMode = () => {
   const wallet = useWallet();
   const { t } = useTranslation();
   const [modes, setModes] = useState<
-    { name: string; label: string; disabled?: boolean }[]
+    { name: string; label: string; disabled?: boolean; tip?: string }[]
   >([
     {
       name: 'key',
@@ -26,15 +27,22 @@ const ImportMode = () => {
 
   const loadMnemonics = async () => {
     const accounts = await wallet.getTypedAccounts(KEYRING_CLASS.MNEMONIC);
-
-    // if (!accounts?.length) {
-    modes.splice(1, 0, {
-      name: 'mnemonics',
-      label: t('Import via Mnemonic'),
-      disabled: accounts.length > 0,
-    });
+    if (accounts.length <= 0) {
+      modes.splice(1, 0, {
+        name: 'mnemonics',
+        label: t('Import via Mnemonic'),
+        disabled: accounts.length > 0,
+        tip: t('MnemonicDisableTip'),
+      });
+    } else {
+      modes.push({
+        name: 'mnemonics',
+        label: t('Import via Mnemonic'),
+        disabled: accounts.length > 0,
+        tip: t('MnemonicDisableTip'),
+      });
+    }
     setModes([...modes]);
-    // }
   };
 
   useEffect(() => {
@@ -81,14 +89,18 @@ const ImportMode = () => {
       </header>
       <div className="pt-32 px-20">
         {modes.map((e) => (
-          <FieldCheckbox
-            key={e.name}
-            disable={e.disabled}
-            checked={e.name === currentMode}
-            onChange={(checked) => chooseImportMode(e.name, checked)}
-          >
-            <div>{e.label}</div>
-          </FieldCheckbox>
+          <Tooltip title={e.tip} key={e.name}>
+            <div>
+              <FieldCheckbox
+                key={e.name}
+                disable={e.disabled}
+                checked={e.name === currentMode}
+                onChange={(checked) => chooseImportMode(e.name, checked)}
+              >
+                <div>{e.label}</div>
+              </FieldCheckbox>
+            </div>
+          </Tooltip>
         ))}
       </div>
     </StrayPageWithButton>
