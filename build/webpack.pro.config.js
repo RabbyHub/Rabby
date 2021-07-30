b97a7436ab357891c9617c6d7fc02ac58f6a6d4e
Paul Verest <enide.github@gmail.com>
2021-07-30 18:34:30 +0800

    fix #155 WARNING in asset size limit (#156)
    * fix #155 WARNING in asset size limit
    
    * remove hints: false,

--- build/webpack.pro.config.js
+++ build/webpack.pro.config.js
@@ -4,6 +4,10 @@ const webpack = require('webpack');
 const config = {
   mode: 'production',
   devtool: false,
+  performance: {
+    maxEntrypointSize: 2500000,
+    maxAssetSize: 2500000
+  },
   plugins: [
     // new BundleAnalyzerPlugin(),
     new webpack.DefinePlugin({
