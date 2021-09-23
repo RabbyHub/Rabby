0adfda5e0583b084e682a04fbb425ba91993ca6c
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2021-09-23 13:48:08 +0800

    fix: can not request approval if user reject previous

--- src/background/utils/promiseFlow.ts
+++ src/background/utils/promiseFlow.ts
@@ -3,8 +3,9 @@ import compose from 'koa-compose';
 export default class PromiseFlow {
   private _tasks: ((args: any) => void)[] = [];
   _context: any = {};
+  requestedApproval = false;
 
-  use(fn) {
+  use(fn): PromiseFlow {
     if (typeof fn !== 'function') {
       throw new Error('promise need function to handle');
     }
