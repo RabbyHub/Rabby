8452ea2959cbb252d3b96cc925dda9b9abf5ae6a
maxxu <xuzhebin2015@gmail.com>
2021-10-28 11:11:56 +0800

    fix: fix conflict

---
+++ src/ui/views/WalletConnect/Component/OpenApiModal.tsx
@@ -0,0 +1,86 @@
+import React, { useEffect } from 'react';
+import { Form, Input, Button } from 'antd';
+import { useTranslation } from 'react-i18next';
+import { useWallet } from 'ui/utils';
+import { Modal } from 'ui/component';
+import { INITIAL_OPENAPI_URL } from 'consts';
+
+const OpenApiModal = ({
+  visible,
+  onFinish,
+  onCancel,
+}: {
+  visible: boolean;
+  onFinish(): void;
+  onCancel(): void;
+}) => {
+  const { useForm } = Form;
+  const [form] = useForm<{ host: string }>();
+  const wallet = useWallet();
+  const { t } = useTranslation();
+
+  const init = async () => {
+    const currentHost = await wallet.openapi.getHost();
+
+    form.setFieldsValue({
+      host: currentHost,
+    });
+  };
+
+  const handleSubmit = async ({ host }: { host: string }) => {
+    await wallet.openapi.setHost(host);
+    onFinish();
+  };
+
+  const restoreInitial = () => {
+    form.setFieldsValue({
+      host: INITIAL_OPENAPI_URL,
+    });
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  return (
+    <Modal
+      title={t('Bridge Service URL')}
+      visible={visible}
+      onCancel={onCancel}
+      className="openapi-modal"
+    >
+      <Form onFinish={handleSubmit} form={form}>
+        <Form.Item
+          name="host"
+          rules={[
+            { required: true, message: t('Please input openapi host') },
+            {
+              pattern: /^((https|http)?:\/\/)[^\s]+\.[^\s]+/,
+              message: t('Please check your host'),
+            },
+          ]}
+        >
+          <Input placeholder="Host" size="large" autoFocus spellCheck={false} />
+        </Form.Item>
+        {form.getFieldValue('host') !== INITIAL_OPENAPI_URL && (
+          <div className="flex justify-end">
+            <Button type="link" onClick={restoreInitial} className="restore">
+              {t('Restore initial setting')}
+            </Button>
+          </div>
+        )}
+        <div className="flex justify-center mt-24">
+          <Button
+            type="primary"
+            size="large"
+            htmlType="submit"
+            className="w-[200px]"
+          >
+            {t('Save')}
+          </Button>
+        </div>
+      </Form>
+    </Modal>
+  );
+};
+export default OpenApiModal;
