fe3f24d00488203dbc0499e8c7fc090a38860f1f
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2021-11-03 16:51:33 +0800

    fix: multi address in on keyring update balance

--- src/ui/component/AddressList/index.tsx
+++ src/ui/component/AddressList/index.tsx
@@ -35,12 +35,22 @@ const AddressList: any = forwardRef(
     }: AddressListProps,
     ref
   ) => {
-    const addressItems = useRef(new Array(list.length));
+    const addressItems = useRef({});
+    list.forEach((group) => {
+      if (addressItems.current[group.type]) {
+        addressItems.current[group.type] = [
+          ...addressItems.current[group.type],
+          ...new Array(group.accounts.length),
+        ];
+      } else {
+        addressItems.current[group.type] = new Array(group.accounts.length);
+      }
+    });
 
     const updateAllBalance = () => {
       const q: Promise<void>[] = [];
-      addressItems.current.forEach((item) => {
-        item.updateBalance();
+      Object.values(addressItems.current).forEach((arr: any) => {
+        q.push(...arr.map((el) => el.updateBalance()));
       });
       return Promise.all(q);
     };
@@ -48,6 +58,40 @@ const AddressList: any = forwardRef(
     useImperativeHandle(ref, () => ({
       updateAllBalance,
     }));
+    const GroupItem = ({ group }: { group: DisplayedKeryring }) => {
+      return (
+        <li>
+          <ul className="addresses">
+            {group.accounts.map((account, index) => (
+              <AddressItem
+                key={account.address}
+                account={{ ...account, type: group.type }}
+                keyring={group.keyring}
+                ActionButton={ActionButton}
+                onClick={onClick}
+                hiddenAddresses={hiddenAddresses}
+                currentAccount={currentAccount}
+                showAssets
+                ref={(el) => {
+                  let i: number | null = index;
+                  while (
+                    i !== null &&
+                    i < addressItems.current[group.keyring.type].length
+                  ) {
+                    if (addressItems.current[group.keyring.type][i]) {
+                      i++;
+                    } else {
+                      addressItems.current[group.keyring.type][i] = el;
+                      i = null;
+                    }
+                  }
+                }}
+              />
+            ))}
+          </ul>
+        </li>
+      );
+    };
 
     return (
       <ul className={`address-group-list ${action}`}>
@@ -55,26 +99,8 @@ const AddressList: any = forwardRef(
           .sort((a, b) => {
             return SORT_WEIGHT[a.type] - SORT_WEIGHT[b.type];
           })
-          .map((group, index) => (
-            <li key={index}>
-              <ul className="addresses">
-                {group.accounts.map((account) => (
-                  <AddressItem
-                    key={`${index}-${account.address}`}
-                    account={{ ...account, type: group.type }}
-                    keyring={group.keyring}
-                    ActionButton={ActionButton}
-                    onClick={onClick}
-                    hiddenAddresses={hiddenAddresses}
-                    currentAccount={currentAccount}
-                    showAssets
-                    ref={(el) => {
-                      addressItems.current[index] = el;
-                    }}
-                  />
-                ))}
-              </ul>
-            </li>
+          .map((group) => (
+            <GroupItem key={group.type} group={group} />
           ))}
       </ul>
     );
