7f4b4a0549189047ab4216047d2bcefb663ed274
Александр <rabby_on_the_mars@yahoo.com>
2021-12-03 10:53:49 +0800

    feat: modify icon pl

--- changeLogs/index.ts
+++ changeLogs/index.ts
@@ -2,6 +2,9 @@ import version0161 from './test0161.md';
 const version = process.env.release || '0';
 const versionMap = {
   '0.16.1': version0161,
+  '0.16.4': version0161,
+  '0.17': version0161,
+  '0.17.1': version0161,
 };
 export const getUpdateContent = () => {
   return versionMap[version];
