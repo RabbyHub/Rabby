ddc9c472798d08fb076903d65c9e0e8ebe36ddb8
Александр <rabby_on_the_mars@yahoo.com>
2021-12-03 15:42:47 +0800

    feat: change version log

--- changeLogs/index.ts
+++ changeLogs/index.ts
@@ -1,7 +1,7 @@
 import version0170 from './0170.md';
 const version = process.env.release || '0';
 const versionMap = {
-  '0170': version0170,
+  '0.17.0': version0170,
 };
 export const getUpdateContent = () => {
   return versionMap[version];
