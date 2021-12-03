8d3582f1c75489bbfbf04f934d37c9d750b6a12b
Александр <rabby_on_the_mars@yahoo.com>
2021-12-03 15:26:19 +0800

    feat: add 0.17.0 change log

--- changeLogs/index.ts
+++ changeLogs/index.ts
@@ -1,13 +1,10 @@
-import version0161 from './test0161.md';
+import version0170 from './0170.md';
 const version = process.env.release || '0';
 const versionMap = {
-  '0.16.1': version0161,
-  '0.16.4': version0161,
-  '0.17': version0161,
-  '0.17.1': version0161,
+  '0170': version0170,
 };
 export const getUpdateContent = () => {
   return versionMap[version];
 };
 
-export { default as version0161 } from './test0161.md';
+export { default as version0170 } from './0170.md';
