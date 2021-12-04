f0b91c7620d87fc24a2e0778e35bb2c54adddde8
Александр <rabby_on_the_mars@yahoo.com>
2021-12-04 20:43:12 +0800

    feat: prevent rerender

--- src/ui/component/MultiSelectAddressList.tsx
+++ src/ui/component/MultiSelectAddressList.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { FixedSizeList, areEqual } from 'react-window';
+import { FixedSizeList } from 'react-window';
 import { Skeleton } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { FieldCheckbox } from 'ui/component';
@@ -24,7 +24,74 @@ interface MultiSelectAddressListArgs {
   loading?: boolean;
   isPopup?: boolean;
 }
-
+const Row = (props) => {
+  const { data, index, style } = props;
+  const { accounts, others } = data;
+  const { importedAccounts, _value, loading, isPopup, handleToggle } = others;
+  //console.log(accounts, 88);
+  const { t } = useTranslation();
+  const imported =
+    (accounts.length > 0 &&
+      importedAccounts &&
+      importedAccounts.length > 0 &&
+      importedAccounts
+        ?.map((address) => address.toLowerCase())
+        .includes(accounts[index].address.toLowerCase())) ||
+    0;
+  const selected = _value.includes(index + 1);
+  return !loading && accounts[index] ? (
+    <div
+      style={style}
+      key={index}
+      className={isPopup ? 'addresses' : 'hard-address'}
+    >
+      <FieldCheckbox
+        checked={selected}
+        onChange={() => handleToggle(index)}
+        disable={
+          imported && (
+            <span
+              className={clsx(
+                'rounded-full bg-gray-bg text-gray-comment text-12 px-[5px] py-[3px]'
+              )}
+            >
+              {t('Imported')}
+            </span>
+          )
+        }
+      >
+        <AddressItem
+          account={{
+            address: accounts[index].address,
+            type: accounts[index].type,
+            brandName: BRAND_ALIAN_TYPE_TEXT[accounts[index].type],
+          }}
+          noNeedBalance={!imported}
+          showAssets={imported}
+          className="select-address-item"
+          editing={false}
+          showImportIcon={false}
+          index={accounts[index].index}
+          showIndex={true}
+        />
+      </FieldCheckbox>
+    </div>
+  ) : (
+    <div
+      style={style}
+      key={index}
+      className={isPopup ? 'addresses' : 'hard-address'}
+    >
+      <div className={clsx('skeleton items-center', { 'w-[460px]': !isPopup })}>
+        <Skeleton.Input
+          active
+          className="items-center"
+          style={{ width: index % 2 ? 140 : 160 }}
+        />
+      </div>
+    </div>
+  );
+};
 const MultiSelectAddressList = ({
   accounts,
   onChange,
@@ -38,7 +105,6 @@ const MultiSelectAddressList = ({
   loading,
   isPopup,
 }: MultiSelectAddressListArgs) => {
-  const { t } = useTranslation();
   const fixedList = useRef<FixedSizeList>();
   const [_value, , , handleToggle] = useSelectOption<number>({
     onChange,
@@ -53,73 +119,6 @@ const MultiSelectAddressList = ({
       fixedList.current?.scrollToItem(end, 'center');
     }
   }, [loadLength, end]);
-  const Row = (props) => {
-    const { data, index, style, isScrolling } = props;
-    const { accounts } = data;
-    //console.log(accounts, 88);
-    const imported =
-      importedAccounts &&
-      importedAccounts.length > 0 &&
-      importedAccounts
-        ?.map((address) => address.toLowerCase())
-        .includes(accounts[index].address.toLowerCase());
-    const selected = _value.includes(index + 1);
-    //console.log(accounts, 99999);
-    return !loading && accounts[index] ? (
-      <div
-        style={style}
-        key={index}
-        className={isPopup ? 'addresses' : 'hard-address'}
-      >
-        <FieldCheckbox
-          checked={selected}
-          onChange={() => handleToggle(index)}
-          disable={
-            imported && (
-              <span
-                className={clsx(
-                  'rounded-full bg-gray-bg text-gray-comment text-12 px-[5px] py-[3px]'
-                )}
-              >
-                {t('Imported')}
-              </span>
-            )
-          }
-        >
-          <AddressItem
-            account={{
-              address: accounts[index].address,
-              type,
-              brandName: BRAND_ALIAN_TYPE_TEXT[type],
-            }}
-            noNeedBalance={!imported}
-            showAssets={imported}
-            className="select-address-item"
-            editing={false}
-            showImportIcon={false}
-            index={accounts[index].index}
-            showIndex={true}
-          />
-        </FieldCheckbox>
-      </div>
-    ) : (
-      <div
-        style={style}
-        key={index}
-        className={isPopup ? 'addresses' : 'hard-address'}
-      >
-        <div
-          className={clsx('skeleton items-center', { 'w-[460px]': !isPopup })}
-        >
-          <Skeleton.Input
-            active
-            className="items-center"
-            style={{ width: index % 2 ? 140 : 160 }}
-          />
-        </div>
-      </div>
-    );
-  };
   const onItemsRendered = ({ overscanStartIndex, overscanStopIndex }) => {
     if (overscanStopIndex + 5 > accounts.length) {
       loadMoreItems && loadMoreItems();
@@ -130,7 +129,14 @@ const MultiSelectAddressList = ({
       height={isPopup ? 500 : 340}
       width={isPopup ? 360 : 460}
       itemData={{
-        accounts,
+        accounts: accounts,
+        others: {
+          importedAccounts,
+          _value,
+          loading,
+          isPopup,
+          handleToggle,
+        },
       }}
       itemCount={accounts.length}
       itemSize={60}
