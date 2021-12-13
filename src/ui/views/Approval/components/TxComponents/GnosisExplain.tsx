b2d4709dbe8859cc3ba34300c73eeebbcef88d16
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2021-12-13 22:02:49 +0800

    feat: gnosis internal transaction ui

--- src/ui/views/Approval/components/TxComponents/GnosisExplain.tsx
+++ src/ui/views/Approval/components/TxComponents/GnosisExplain.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ExplainTxResponse, Tx } from 'background/service/openapi';
 import { CHAINS, CHAINS_ENUM } from 'consts';
+import IconGnosis from 'ui/assets/walletlogo/gnosis.png';
+import AddressViewer from 'ui/component/AddressViewer';
 import { TxTypeComponent } from '../SignTx';
 
 interface GnosisExplainProps {
@@ -15,9 +17,13 @@ const GnosisExplain = ({ data, chainEnum, raw, tx }: GnosisExplainProps) => {
   const handleChange = () => {
     // TODO
   };
-
+  console.log(data, tx);
   return (
     <div className="gonsis-explain">
+      <div className="gnosis-address">
+        <img src={IconGnosis} className="icon icon-gnosis" />
+        <AddressViewer address={tx.to} showArrow={false} />
+      </div>
       <TxTypeComponent
         txDetail={data}
         chain={chain}
