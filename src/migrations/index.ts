47a7184c326ad5efae580bdb6038301d9135fcfe
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2022-01-07 17:34:17 +0800

    chore: rm useless code

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,3 @@
-import { browser } from 'webextension-polyfill-ts';
 import { storage } from '@/background/webapi';
 import * as migrations from './migrations';
 
@@ -17,7 +16,6 @@ const sortedMigrations = Object.values(migrations).sort((a, b) => {
 });
 
 export default async function (currentDataVersion: number) {
-  console.log('currentDataVersion', currentDataVersion);
   let result: any = {};
   let changed = false;
   let dataVersion = currentDataVersion;
@@ -33,17 +31,13 @@ export default async function (currentDataVersion: number) {
     if (migration.version > currentDataVersion) {
       changed = true;
       const migrationResult = await migration.migrator(result);
-      console.log('migrationResult', migrationResult);
       result = Object.assign({}, result, migrationResult);
       dataVersion = migration.version;
     }
   }
   if (!changed) return;
   for (const key in result) {
-    console.log(key, result[key]);
     await storage.set(key, result[key]);
   }
-  // await storage.set('dataVersion', dataVersion);
-  const pre = await storage.get('preference');
-  console.log('pre', pre);
+  await storage.set('dataVersion', dataVersion);
 }
