e8266540b77d91511e2552a346024521c93aef52
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2022-01-12 16:59:07 +0800

    feat: support moonbeam chain

--- src/background/service/openapi.ts
+++ src/background/service/openapi.ts
@@ -686,6 +686,15 @@ class OpenApiService {
         wrapped_token_id: '',
         symbol: '',
       },
+      {
+        community_id: 1284,
+        id: 'mobm',
+        logo_url: '',
+        name: '',
+        native_token_id: '',
+        wrapped_token_id: '',
+        symbol: '',
+      },
     ]);
   };
 
