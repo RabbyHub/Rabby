9c72822a3b06837086735c76d44eac407b5c792e
vvvvvv1vvvvvv <86296331+vvvvvv1vvvvvv@users.noreply.github.com>
2022-03-10 16:59:48 +0800

    fix: route gurd not worked (#586)
    * fix: privateRoute guard not worked
    
    * fix: state not refreshed

--- src/ui/component/PrivateRoute.tsx
+++ src/ui/component/PrivateRoute.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { useWallet } from 'ui/utils';
 
-const PrivateRoute = ({ children, ...rest }) => {
+const Wrap = ({ children }) => {
   const wallet = useWallet();
+  const [isBooted, setIsBooted] = useState(false);
+  const [isUnlocked, setIsUnlocked] = useState(false);
+  const [isReady, setIsReady] = useState(false);
+  const to = !isBooted ? '/welcome' : !isUnlocked ? '/unlock' : null;
 
-  return (
-    <Route
-      {...rest}
-      render={() => {
-        const to = !wallet.isBooted()
-          ? '/welcome'
-          : !wallet.isUnlocked()
-          ? '/unlock'
-          : null;
+  const init = async () => {
+    setIsBooted(await wallet.isBooted());
+    setIsUnlocked(await wallet.isUnlocked());
+    setIsReady(true);
+  };
 
-        return !to ? children : <Redirect to={to} />;
-      }}
-    />
-  );
+  useEffect(() => {
+    init();
+  }, []);
+
+  if (!isReady) return <></>;
+  return !to ? children : <Redirect to={to} />;
+};
+
+const PrivateRoute = ({ children, ...rest }) => {
+  return <Route {...rest} render={() => <Wrap>{children}</Wrap>} />;
 };
 
 export default PrivateRoute;
