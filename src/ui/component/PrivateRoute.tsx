0d1ce7819ed6ea5b0b249dfafa0b62392066318d
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2022-03-10 12:26:01 +0800

    fix: privateRoute guard not worked

--- src/ui/component/PrivateRoute.tsx
+++ src/ui/component/PrivateRoute.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { useWallet } from 'ui/utils';
 
 const PrivateRoute = ({ children, ...rest }) => {
   const wallet = useWallet();
+  const [isBooted, setIsBooted] = useState(false);
+  const [isUnlocked, setIsUnlocked] = useState(false);
+  const [isReady, setIsReady] = useState(false);
+
+  const init = async () => {
+    setIsBooted(await wallet.isBooted());
+    setIsUnlocked(await wallet.isUnlocked());
+    setIsReady(true);
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  if (!isReady) return <></>;
 
   return (
     <Route
       {...rest}
       render={() => {
-        const to = !wallet.isBooted()
-          ? '/welcome'
-          : !wallet.isUnlocked()
-          ? '/unlock'
-          : null;
+        const to = !isBooted ? '/welcome' : !isUnlocked ? '/unlock' : null;
 
         return !to ? children : <Redirect to={to} />;
       }}
