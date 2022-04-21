08e9a7fb874d7bab3029b69384e7c2462dbd24c9
Hodor <lost1q84@gmail.com>
2022-04-21 14:52:02 +0800

    feat: hide small assets (#681)

--- src/ui/views/Dashboard/components/AssetsList.tsx
+++ src/ui/views/Dashboard/components/AssetsList.tsx
@@ -1,19 +1,42 @@
 import clsx from 'clsx';
-import React, { useRef, useEffect } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { FixedSizeList } from 'react-window';
-import { TokenWithChain } from 'ui/component';
-import { splitNumberByStep, useHover, openInTab } from 'ui/utils';
 import { SvgIconLoading } from 'ui/assets';
+import IconArrowUp from 'ui/assets/arrow-up.svg';
 import IconOpenDeFi from 'ui/assets/dashboard/opendefi.png';
+import { TokenWithChain } from 'ui/component';
+import { openInTab, splitNumberByStep, useHover } from 'ui/utils';
 
 const Row = (props) => {
-  const { data, index, style } = props;
+  const { data, index, style, isExpand, setIsExpand } = props;
   const token = data[index];
   const [isHovering, hoverProps] = useHover();
   const handleGotoProfile = () => {
     openInTab(token?.site_url);
   };
+
+  if (token.isShowExpand) {
+    return (
+      <div
+        className="filter"
+        style={style}
+        onClick={() => setIsExpand((v) => !v)}
+      >
+        {!isExpand ? (
+          <div className="flex justify-center items-center">
+            {'Small deposits are hidden (<1%)'}
+            <img src={IconArrowUp} className="rotate-180"></img>
+          </div>
+        ) : (
+          <div className="flex justify-center items-center">
+            {'Hide small deposits (<1%)'}
+            <img src={IconArrowUp}></img>
+          </div>
+        )}
+      </div>
+    );
+  }
 
   return (
     <div
@@ -45,6 +68,37 @@ const Row = (props) => {
     </div>
   );
 };
+const useExpandList = (assets) => {
+  const [isExpand, setIsExpand] = useState(false);
+  const total = useMemo(
+    () => assets.reduce((t, item) => (item.net_usd_value || 0) + t, 0),
+    [assets]
+  );
+  const filterPrice = total / 100;
+  const isShowExpand = assets.some((item) => item.net_usd_value < filterPrice);
+
+  const filterList = useMemo(() => {
+    let result = isExpand
+      ? assets
+      : assets.filter((item) => item.net_usd_value >= filterPrice);
+    if (isShowExpand) {
+      result = result.concat([
+        {
+          isShowExpand: true,
+        },
+      ]);
+    }
+    return result;
+  }, [isExpand, assets, isShowExpand]);
+
+  return {
+    isExpand,
+    setIsExpand,
+    filterList,
+    filterPrice,
+    isShowExpand,
+  };
+};
 const AssetsList = ({
   assets,
   defiAnimate,
@@ -53,9 +107,11 @@ const AssetsList = ({
 }) => {
   const { t } = useTranslation();
   const fixedList = useRef<FixedSizeList>();
+  const { isExpand, setIsExpand, filterList } = useExpandList(assets);
   useEffect(() => {
     if (!isloading && assets.length > 0 && defiAnimate.includes('fadeIn')) {
       fixedList.current?.scrollToItem(0);
+      setIsExpand(false);
     }
   }, [defiAnimate, isloading, assets]);
   if (!startAnimate) {
@@ -70,17 +126,21 @@ const AssetsList = ({
         </div>
       )}
       {!isloading && assets.length > 0 ? (
-        <FixedSizeList
-          height={468}
-          width="100%"
-          itemData={assets}
-          itemCount={assets.length}
-          itemSize={48}
-          ref={fixedList}
-          style={{ zIndex: 10, overflowX: 'hidden', paddingBottom: 50 }}
-        >
-          {Row}
-        </FixedSizeList>
+        <>
+          <FixedSizeList
+            height={424}
+            width="100%"
+            itemData={filterList}
+            itemCount={filterList.length}
+            itemSize={48}
+            ref={fixedList}
+            style={{ zIndex: 10, overflowX: 'hidden' }}
+          >
+            {(props) => (
+              <Row {...props} isExpand={isExpand} setIsExpand={setIsExpand} />
+            )}
+          </FixedSizeList>
+        </>
       ) : (
         <div className="no-data">
           <img className="w-[100px] h-[100px]" src="./images/nodata-tx.png" />
