dbd897260d1412d1002c895fbaf2d4a9ba40e3d0
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2022-04-29 18:22:17 +0800

    fix: remove toast of unconnect website switch chain

--- src/ui/views/Dashboard/components/CurrentConnection/index.tsx
+++ src/ui/views/Dashboard/components/CurrentConnection/index.tsx
@@ -1,7 +1,7 @@
 import { message, Tooltip } from 'antd';
 import { ConnectedSite } from 'background/service/permission';
 import clsx from 'clsx';
-import { CHAINS, CHAINS_ENUM } from 'consts';
+import { CHAINS_ENUM } from 'consts';
 import React, { memo, useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import IconDisconnect from 'ui/assets/icon-disconnect.svg';
@@ -36,17 +36,6 @@ export const CurrentConnection = memo(() => {
       ...site!,
       chain,
     };
-    if (!site?.isConnected) {
-      const chainName = CHAINS[chain].name;
-      message.success({
-        icon: <i />,
-        content: (
-          <span className="text-white">
-            {t(`${chainName} has been selected for the current Dapp`)}
-          </span>
-        ),
-      });
-    }
     setSite(_site);
     setVisible(false);
     await wallet.setSite(_site);
