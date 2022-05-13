3573590a2b4ffe8fe0aea829dcbe0dc6fcee97a1
vvvvvv1vvvvvv <86296331+vvvvvv1vvvvvv@users.noreply.github.com>
2022-05-13 11:15:15 +0800

    chore: upload sourcemap files to sentry (#744)

---
+++ build/webpack.sourcemap.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const path = require('path');
+const fs = require('fs-extra');
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const SentryCliPlugin = require('@sentry/webpack-plugin');
+
+const PROJECT_ROOT = path.resolve(__dirname, '..');
+const manifestPath = path.resolve(PROJECT_ROOT, '_raw', 'manifest.json');
+const manifest = fs.readJSONSync(manifestPath);
+
+const config = {
+  mode: 'production',
+  devtool: false,
+  performance: {
+    maxEntrypointSize: 2500000,
+    maxAssetSize: 2500000,
+  },
+  plugins: [
+    new webpack.SourceMapDevToolPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.BUILD_ENV': JSON.stringify('PRO'),
+    }),
+    new SentryCliPlugin({
+      include: './dist',
+      ignoreFile: '.sentrycliignore',
+      ignore: ['node_modules', 'webpack.config.js'],
+      configFile: 'sentry.properties',
+      release: manifest.version,
+    }),
+  ],
+};
+
+module.exports = config;
