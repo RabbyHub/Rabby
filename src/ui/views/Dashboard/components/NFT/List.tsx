7018f91adad71f1a42bb7594e7eccee793a9d692
Hodor <lost1q84@gmail.com>
2022-06-24 11:59:48 +0800

    feat: change nft collection list params

--- src/ui/views/Dashboard/components/NFT/List.tsx
+++ src/ui/views/Dashboard/components/NFT/List.tsx
@@ -5,6 +5,7 @@ import {
 } from '@/background/service/openapi';
 import { useWallet } from '@/ui/utils';
 import clsx from 'clsx';
+import _ from 'lodash';
 import { groupBy, keyBy } from 'lodash';
 import React, { useEffect, useMemo, useState } from 'react';
 import { getChain } from 'utils';
@@ -27,18 +28,22 @@ const NFTListContainer = ({
   const [isLoading, setIsLoading] = useState(false);
   const [list, setList] = useState<NFTItem[]>([]);
   const [collectionList, setCollectionList] = useState<UserCollection[]>([]);
-  const [idx, setIdx] = useState(1);
+  const [isDirtyList, setIsDirtyList] = useState(true);
+  const [isDirtyCollection, setIsDirtyCollection] = useState(true);
   const wallet = useWallet();
-
-  const fetchCollection = useMemo(() => wallet.openapi.listCollection(), []);
 
   const fetchData = async (id: string, isAll = false) => {
     try {
       setIsLoading(true);
-      const [data, collections]: [NFTItem[], Collection[]] = await Promise.all([
-        wallet.openapi.listNFT(id, isAll),
-        fetchCollection,
-      ]);
+
+      const data = await wallet.openapi.listNFT(id, isAll);
+      const ids = _.uniq(data.map((item) => item.collection_id)).filter(
+        (item): item is string => !!item
+      );
+      const collections = await wallet.openapi.listCollection({
+        collection_ids: ids.join(','),
+      });
+
       const dict = keyBy(collections, 'id');
       const list = data
         .filter((item) => getChain(item.chain))
@@ -69,8 +74,10 @@ const NFTListContainer = ({
       }, [] as UserCollection[]);
       if (isAll) {
         setList(list);
+        setIsDirtyList(false);
       } else {
         setCollectionList(collectionList);
+        setIsDirtyCollection(false);
       }
     } catch (e) {
       console.error(e);
@@ -82,19 +89,20 @@ const NFTListContainer = ({
   useEffect(() => {
     setList([]);
     setCollectionList([]);
-    setIdx(idx + 1);
+    setIsDirtyList(true);
+    setIsDirtyCollection(true);
   }, [address]);
 
   useEffect(() => {
     if (address && animate?.indexOf('fadeIn') !== -1) {
-      if (type === 'nft') {
+      if (type === 'nft' && isDirtyList) {
         fetchData(address, true);
       }
-      if (type === 'collection') {
+      if (type === 'collection' && isDirtyCollection) {
         fetchData(address, false);
       }
     }
-  }, [address, type, animate, idx]);
+  }, [address, type, animate, isDirtyList, isDirtyCollection]);
 
   if (!startAnimate) {
     return <></>;
