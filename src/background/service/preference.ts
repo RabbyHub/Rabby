247d71dac27718cb7672361d5b0d6256f5f555b6
qingwei-li <cinwell.li@gmail.com>
2022-07-14 17:28:22 +0800

    fix: this.store is undefined

--- src/background/service/preference.ts
+++ src/background/service/preference.ts
@@ -271,7 +271,7 @@ class PreferenceService {
   };
 
   getCurrentAccount = (): Account | undefined | null => {
-    const account = cloneDeep(this.store.currentAccount);
+    const account = cloneDeep(this.store?.currentAccount);
     if (!account) return account;
     return {
       ...account,
