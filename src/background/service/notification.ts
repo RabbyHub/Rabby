9ab7e4ee666371fd35a4ddeff8dfb3e5137b89fe
vvvvvv1vvvvvv <86296331+vvvvvv1vvvvvv@users.noreply.github.com>
2022-08-09 18:05:30 +0800

    fix: popup will be auto reject in linux (#910)
    * fix: popup will be auto reject in linux
    
    * chore: + comment

--- src/background/service/notification.ts
+++ src/background/service/notification.ts
@@ -2,7 +2,7 @@ import Events from 'events';
 import { ethErrors } from 'eth-rpc-errors';
 import { EthereumProviderError } from 'eth-rpc-errors/dist/classes';
 import { winMgr } from 'background/webapi';
-import { CHAINS } from 'consts';
+import { CHAINS, IS_LINUX, IS_CHROME } from 'consts';
 import { browser } from 'webextension-polyfill-ts';
 
 type IApprovalComponents = typeof import('@/ui/views/Approval/components');
@@ -68,6 +68,11 @@ class NotificationService extends Events {
     });
 
     winMgr.event.on('windowFocusChange', (winId: number) => {
+      if (IS_CHROME && winId === chrome.windows.WINDOW_ID_NONE && IS_LINUX) {
+        // When sign on Linux, will focus on -1 first then focus on sign window
+        return;
+      }
+
       if (this.notifiWindowId && winId !== this.notifiWindowId) {
         if (
           this.currentApproval &&
