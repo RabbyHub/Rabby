08dcfac87e0374fa843e7bba491356aa2531f677
heisenberg <heisenberg-2077@outlook.com>
2022-09-06 19:36:34 +0800

    fix: pageProvider icon

--- build/webpack.common.config.js
+++ build/webpack.common.config.js
@@ -150,13 +150,34 @@ const config = {
       },
       {
         test: /\.svg$/,
-        use: [
-          '@svgr/webpack',
+        oneOf: [
           {
-            loader: 'url-loader',
-            options: {
-              limit: false,
-            },
+            include: [
+              path.resolve(
+                __dirname,
+                '../src/content-script/pageProvider/assets'
+              ),
+            ],
+            use: [
+              '@svgr/webpack',
+              {
+                loader: 'url-loader',
+                options: {
+                  limit: true,
+                },
+              },
+            ],
+          },
+          {
+            use: [
+              '@svgr/webpack',
+              {
+                loader: 'url-loader',
+                options: {
+                  limit: false,
+                },
+              },
+            ],
           },
         ],
       },
