82f1b5d1dbb3d18765cba5ef9ad9b5a77fcd322c
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2022-09-21 15:32:04 +0800

    chore: lazy load motamo script

--- _raw/vendor/matomo.js
+++ _raw/vendor/matomo.js
@@ -3,13 +3,15 @@ var _paq = (window._paq = window._paq || []);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function () {
-  var u = 'https://matomo.debank.com/';
-  _paq.push(['setTrackerUrl', u + 'matomo.php']);
-  _paq.push(['setSiteId', '2']);
-  var d = document,
-    g = d.createElement('script'),
-    s = d.getElementsByTagName('script')[0];
-  g.async = true;
-  g.src = u + 'matomo.js';
-  s.parentNode.insertBefore(g, s);
+  setTimeout(() => {
+    var u = 'https://matomo.debank.com/';
+    _paq.push(['setTrackerUrl', u + 'matomo.php']);
+    _paq.push(['setSiteId', '2']);
+    var d = document,
+      g = d.createElement('script'),
+      s = d.getElementsByTagName('script')[0];
+    g.async = true;
+    g.src = u + 'matomo.js';
+    s.parentNode.insertBefore(g, s);
+  }, 500)
 })();
