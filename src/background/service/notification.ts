93adaca549f2debc28caad02976ff8a8301fb562
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2022-11-01 18:48:34 +0800

    fix: activeFirstApproval sometimes not working

--- src/background/service/notification.ts
+++ src/background/service/notification.ts
@@ -94,8 +94,12 @@ class NotificationService extends Events {
     });
   }
 
-  activeFirstApproval = () => {
-    if (this.notifiWindowId !== null) {
+  activeFirstApproval = async () => {
+    const windows = await browser.windows.getAll();
+    const existWindow = windows.find(
+      (window) => window.id === this.notifiWindowId
+    );
+    if (this.notifiWindowId !== null && !!existWindow) {
       browser.windows.update(this.notifiWindowId, {
         focused: true,
       });
@@ -106,7 +110,7 @@ class NotificationService extends Events {
 
     const approval = this.approvals[0];
     this.currentApproval = approval;
-    this.openNotification(approval.winProps);
+    this.openNotification(approval.winProps, true);
   };
 
   deleteApproval = (approval) => {
@@ -294,9 +298,12 @@ class NotificationService extends Events {
     this.isLocked = true;
   };
 
-  openNotification = (winProps) => {
-    if (this.isLocked) return;
-    this.lock();
+  openNotification = (winProps, ignoreLock = false) => {
+    // Only use ignoreLock flag when approval exist but no notification window exist
+    if (!ignoreLock) {
+      if (this.isLocked) return;
+      this.lock();
+    }
     if (this.notifiWindowId !== null) {
       winMgr.remove(this.notifiWindowId);
       this.notifiWindowId = null;
