090deb5024396d2fcce4b443720deaebbae3816b
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2022-11-04 14:47:39 +0800

    fix: + external resource blacklist to prevent warning from chrome

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -43,6 +43,20 @@ export const getOriginFromUrl = (url: string) => {
   return urlObj.origin;
 };
 
+/**
+ *
+ * @param url (exchange.pancakeswap.finance/blabla)
+ * @returns (pancakeswap.finance)
+ */
+export const getMainDomain = (url: string) => {
+  const origin = getOriginFromUrl(url);
+  const arr = origin.split('.');
+  const mainDomainWithPath = [arr[arr.length - 2], arr[arr.length - 1]].join(
+    '.'
+  );
+  return mainDomainWithPath.replace(/^https?:\/\//, '');
+};
+
 export const resemblesETHAddress = (str: string): boolean => {
   return str.length === 42;
 };
