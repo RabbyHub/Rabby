eb8ea7c62838a5f3e06065bf07083fc612b64728
heisenberg <heisenberg-2077@outlook.com>
2022-11-08 13:17:25 +0800

    fix: onekey

--- src/ui/utils/keyrings/onekey.ts
+++ src/ui/utils/keyrings/onekey.ts
@@ -1,3 +1,4 @@
+import { sleep } from '@/background/utils';
 import OneKeyConnect from '@onekeyfe/connect';
 
 const ONEKEY_CONNECT_MANIFEST = {
@@ -6,23 +7,49 @@ const ONEKEY_CONNECT_MANIFEST = {
 };
 
 export class OneKeyKeyring {
+  initiated = false;
+
   async init() {
-    return OneKeyConnect.manifest(ONEKEY_CONNECT_MANIFEST);
+    this.initiated = false;
+    try {
+      await OneKeyConnect.init({
+        manifest: ONEKEY_CONNECT_MANIFEST,
+      });
+      this.initiated = true;
+    } catch (e) {
+      // ignore init error
+      this.close();
+      this.init();
+    }
+    return;
+  }
+  close() {
+    OneKeyConnect.dispose();
+  }
+
+  async waitInit() {
+    while (!this.initiated) {
+      await sleep(500);
+    }
   }
 
   async getPublicKey(params) {
+    await this.waitInit();
     return OneKeyConnect.getPublicKey(params);
   }
 
   async ethereumSignTransaction(params) {
+    await this.waitInit();
     return OneKeyConnect.ethereumSignTransaction(params);
   }
 
   async ethereumSignMessage(params) {
+    await this.waitInit();
     return OneKeyConnect.ethereumSignMessage(params);
   }
 
   async ethereumSignMessageEIP712(params) {
+    await this.waitInit();
     return OneKeyConnect.ethereumSignMessageEIP712(params);
   }
 }
