2f8545703445ed7941220c7858e4bed52879e210
DMY <147dmy@gmail.com>
2022-11-18 19:28:03 +0800

    docs: 0.57.0 changelog

--- changeLogs/index.ts
+++ changeLogs/index.ts
@@ -4,6 +4,7 @@ import version0460 from './0460.md';
 import version0490 from './0490.md';
 import version0530 from './0530.md';
 import version0560 from './0560.md';
+import version0570 from './0570.md';
 
 const version = process.env.release || '0';
 const versionMap = {
@@ -14,6 +15,7 @@ const versionMap = {
   '0.49.1': version0490,
   '0.53.0': version0530,
   '0.56.0': version0560,
+  '0.57.0': version0570,
 };
 export const getUpdateContent = () => {
   return versionMap[version];
