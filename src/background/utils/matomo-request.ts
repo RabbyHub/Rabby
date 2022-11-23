be7cd197f8b98dc8869b7724fa9a9d6cdf66c082
heisenberg <heisenberg-2077@outlook.com>
2022-11-23 10:49:40 +0800

    fix: track

--- src/background/utils/matomo-request.ts
+++ src/background/utils/matomo-request.ts
@@ -1,38 +1,35 @@
 import { storage } from 'background/webapi';
-import { nanoid } from 'nanoid';
+import { customAlphabet, nanoid } from 'nanoid';
 
 const ANALYTICS_PATH = 'https://matomo.debank.com/matomo.php';
+const genCid = customAlphabet('1234567890abcdef', 16);
 
-async function postData(url = '', data: RequestInit['body']) {
-  const response = await fetch(url, {
+async function postData(url = '', params: URLSearchParams) {
+  const response = await fetch(`${url}?${params.toString()}`, {
     method: 'POST',
-    mode: 'no-cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow',
-    referrerPolicy: 'no-referrer',
-    body: data,
   });
 
   return response;
 }
 const getParams = async () => {
-  let cid = await storage.get('cid');
+  let cid = await storage.get('cid_v2');
   if (!cid) {
-    cid = nanoid();
-    storage.set('cid', cid);
+    cid = genCid();
+    storage.set('cid_v2', cid);
   }
 
   const gaParams = new URLSearchParams();
   gaParams.append('idsite', '2');
   gaParams.append('rec', '1');
-  gaParams.append('apiv', '1');
-  gaParams.append('url', location.href);
+  gaParams.append('url', encodeURI(location.href));
   gaParams.append('_id', cid);
   gaParams.append('rand', nanoid());
+  gaParams.append('ca', '1');
+  gaParams.append('h', new Date().getHours().toString());
+  gaParams.append('m', new Date().getMinutes().toString());
+  gaParams.append('s', new Date().getSeconds().toString());
+  gaParams.append('cookie', '0');
+  gaParams.append('send_image', '0');
 
   return gaParams;
 };
@@ -48,18 +45,25 @@ export const matomoRequestEvent = async (
 ) => {
   const params = await getParams();
 
-  params.append('action_name', [data.category, data.action].join('/'));
-  const cvar = {};
+  if (data.category) {
+    params.append('e_c', data.category);
+  }
+
+  if (data.action) {
+    params.append('e_a', data.action);
+  }
+
   if (data.label) {
-    cvar['1'] = ['label', data.label];
+    params.append('e_n', data.label);
   }
+
   if (data.value) {
-    cvar['2'] = ['value', data.value];
+    params.append('e_v', data.value as string);
   }
+
   if (data.transport) {
-    cvar['3'] = ['transport', data.transport];
+    params.append('e_i', data.transport);
   }
-  params.append('cvar', JSON.stringify(cvar));
 
   return postData(ANALYTICS_PATH, params);
 };
