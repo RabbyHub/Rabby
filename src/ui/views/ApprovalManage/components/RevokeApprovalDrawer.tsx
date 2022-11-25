3e2e1273f83487feb49ee9602ebf02235db630ba
DMY <147dmy@gmail.com>
2022-11-25 19:41:43 +0800

    fix:display tokenNFT (#1156)

--- src/ui/views/ApprovalManage/components/RevokeApprovalDrawer.tsx
+++ src/ui/views/ApprovalManage/components/RevokeApprovalDrawer.tsx
@@ -239,7 +239,7 @@ export const RevokeApprovalDrawer = (props: {
               <span
                 className={clsx(
                   'mr-[14px] text-[13px] text-gray-subTitle',
-                  item.type === 'token'
+                  item.type !== 'token' && 'hidden'
                 )}
               >
                 {displayApprovalValue}
