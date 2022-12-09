4ad12abbfdf6f597893f39acc4127651421b7f53
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2022-12-09 10:10:32 +0800

    feat: + changelog

--- changeLogs/index.ts
+++ changeLogs/index.ts
@@ -5,6 +5,7 @@ import version0490 from './0490.md';
 import version0530 from './0530.md';
 import version0560 from './0560.md';
 import version0570 from './0570.md';
+import version0610 from './0610.md';
 
 const version = process.env.release || '0';
 const versionMap = {
@@ -16,6 +17,7 @@ const versionMap = {
   '0.53.0': version0530,
   '0.56.0': version0560,
   '0.57.1': version0570,
+  '0.61.0': version0610,
 };
 export const getUpdateContent = () => {
   return versionMap[version];
