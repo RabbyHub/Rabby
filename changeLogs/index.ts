da838bc3e5688e2acfc8c867d87772e2668f468a
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2023-05-06 18:00:28 +0800

    feat: + changelog

--- changeLogs/index.ts
+++ changeLogs/index.ts
@@ -12,6 +12,7 @@ import version0792 from './0792.md';
 import version0800 from './0800.md';
 import version0810 from './0810.md';
 import version0820 from './0820.md';
+import version0830 from './0830.md';
 
 const version = process.env.release || '0';
 const versionMap = {
@@ -30,6 +31,7 @@ const versionMap = {
   '0.80.0': version0800,
   '0.81.0': version0810,
   '0.82.0': version0820,
+  '0.83.0': version0830,
 };
 export const getUpdateContent = () => {
   return versionMap[version];
