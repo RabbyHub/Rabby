96605f611802c38a48b58b9e34bac905d66ccf7c
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2023-05-25 16:02:09 +0800

    feat: connect ui

--- src/ui/views/Approval/components/Connect/RuleResult.tsx
+++ src/ui/views/Approval/components/Connect/RuleResult.tsx
@@ -1,16 +1,17 @@
 import React, { useMemo } from 'react';
+import { Tooltip } from 'antd';
 import { Result } from '@debank/rabby-security-engine';
-import styled from 'styled-components';
-import SecurityLevelTag from '../SecurityEngine/SecurityLevelTag';
 import { Level } from '@debank/rabby-security-engine/dist/rules';
+import styled from 'styled-components';
+import SecurityLevelTag from '../SecurityEngine/SecurityLevelTagNoText';
+import IconEdit from 'ui/assets/editpen.svg';
 
 const RuleResultWrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  align-items: center;
   min-height: 56px;
   padding: 15px 16px;
-  padding-right: 0;
+  padding-right: 24px;
   background: #f5f6fa;
   border: 1px solid #e5e9ef;
   border-radius: 8px;
@@ -18,37 +19,30 @@ const RuleResultWrapper = styled.div`
   position: relative;
 
   .rule-desc {
+    font-weight: 400;
+    font-size: 13px;
+    line-height: 15px;
+    color: #4b4d59;
+    width: 49%;
+  }
+  .rule-value {
+    display: flex;
+    justify-content: flex-end;
+    flex: 1;
     font-weight: 500;
     font-size: 15px;
     line-height: 18px;
     color: #13141a;
-    padding-right: 80px;
-  }
-  .icon-arrow-right {
-    width: 16px;
-    height: 16px;
-    margin-left: 4px;
   }
   .collect-list {
     display: flex;
     flex-wrap: wrap;
+    gap: 8px;
     &-item {
-      display: flex;
-      align-items: center;
-      font-weight: 500;
-      font-size: 13px;
-      line-height: 15px;
-      color: #13141a;
-      margin-right: 12px;
-      margin-top: 8px;
       img {
-        width: 16px;
-        height: 16px;
+        width: 20px;
+        height: 20px;
         border-radius: 100%;
-        margin-right: 4px;
-      }
-      &:nth-last-child(1) {
-        margin-right: 0;
       }
     }
   }
@@ -61,7 +55,9 @@ const RuleResult = ({
   ignored,
   hasSafe,
   hasForbidden,
+  userListResult,
   onSelect,
+  onEditUserList,
 }: {
   rule: { id: string; desc: string; result: Result | null };
   collectList: { name: string; logo_url: string }[];
@@ -69,7 +65,9 @@ const RuleResult = ({
   ignored: boolean;
   hasSafe: boolean;
   hasForbidden: boolean;
+  userListResult?: Result;
   onSelect(rule: { id: string; desc: string; result: Result | null }): void;
+  onEditUserList(): void;
 }) => {
   const handleClick = () => {
     if (!rule.result) return;
@@ -99,32 +97,13 @@ const RuleResult = ({
 
   const ruleDesc = () => {
     if (rule.id === '1004') {
-      return (
-        <div
-          className={
-            collectList.length > 0
-              ? 'text-12 text-gray-subTitle font-normal'
-              : ''
-          }
-        >
-          {collectList.length === 0
-            ? 'Not listed by any community platforms'
-            : `Listed by ${collectList.length} community platform${
-                collectList.length > 1 ? 's' : ''
-              }`}
-        </div>
-      );
+      return <>Listed by</>;
     }
     if (rule.id === '1005') {
-      return (
-        <>
-          {popularLevel === 'high' && 'Popular site with many visitors'}
-          {popularLevel === 'medium' &&
-            'Average level of popularity among users'}
-          {popularLevel === 'low' && 'Low popularity with few visitors'}
-          {popularLevel === 'very_low' && 'Very few visitors to this site'}
-        </>
-      );
+      return <>Site popularity</>;
+    }
+    if (rule.id === '1006' || rule.id === '1007') {
+      return <>My mark</>;
     }
     if (rule.result) {
       if (
@@ -142,28 +121,59 @@ const RuleResult = ({
 
   return (
     <RuleResultWrapper>
-      <div className="flex justify-between items-center w-full">
-        <div className="rule-desc flex items-center">{ruleDesc()}</div>
+      <div className="rule-desc flex items-center">{ruleDesc()}</div>
+      <div className="rule-value">
+        {rule.id === '1004' && (
+          <div className="collect-list">
+            {collectList.length <= 0 && 'None'}
+            {collectList.length > 0 &&
+              collectList.slice(0, 10).map((item) => (
+                <div className="collect-list-item">
+                  <Tooltip
+                    title={item.name}
+                    placement="top"
+                    overlayClassName="rectangle"
+                  >
+                    <img src={item.logo_url} />
+                  </Tooltip>
+                </div>
+              ))}
+          </div>
+        )}
+        {rule.id === '1005' && (
+          <div>
+            {popularLevel === 'high' && 'High'}
+            {popularLevel === 'medium' && 'Medium'}
+            {popularLevel === 'low' && 'Low'}
+            {popularLevel === 'very_low' && 'Very Low'}
+          </div>
+        )}
+        {['1001', '1002', '1003'].includes(rule.id) && rule.result && (
+          <div>{rule.result.value ? 'Yes' : 'No'}</div>
+        )}
+        {(rule.id === '1006' || rule.id === '1007') && (
+          <div className="flex">
+            {!userListResult && 'No mark'}
+            {userListResult && userListResult.id === '1006' && 'Blocked'}
+            {userListResult && userListResult.id === '1007' && 'Trusted'}
+            <img
+              src={IconEdit}
+              className="ml-6 icon icon-edit cursor-pointer"
+              onClick={onEditUserList}
+            />
+          </div>
+        )}
       </div>
-      {rule.id === '1004' && (
-        <div className="collect-list">
-          {collectList.map((item) => (
-            <div className="collect-list-item">
-              <img src={item.logo_url} />
-              {item.name}
-            </div>
-          ))}
-        </div>
-      )}
       {rule.result && !ignored && (
         <SecurityLevelTag
           level={rule.result.level}
           onClick={handleClick}
           translucent={translucent}
+          right="-12px"
         />
       )}
       {rule.result && ignored && (
-        <SecurityLevelTag level="proceed" onClick={handleClick} />
+        <SecurityLevelTag level="proceed" onClick={handleClick} right="-12px" />
       )}
     </RuleResultWrapper>
   );
