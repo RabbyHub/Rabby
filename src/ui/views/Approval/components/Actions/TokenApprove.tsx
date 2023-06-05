b5dd826a2566f60bfcefc70d856c3395d984248e
heisenberg <110591045+heisenberg-2077@users.noreply.github.com>
2023-06-05 16:54:13 +0800

    Update src/ui/views/Approval/components/Actions/TokenApprove.tsx
    Co-authored-by: vvvvvv1vvvvvv <86296331+vvvvvv1vvvvvv@users.noreply.github.com>

--- src/ui/views/Approval/components/Actions/TokenApprove.tsx
+++ src/ui/views/Approval/components/Actions/TokenApprove.tsx
@@ -268,7 +268,7 @@ const TokenApprove = ({
               <SecurityListItem
                 id="1023"
                 engineResult={engineResultMap['1023']}
-                dangerText="Risk exposure  < $10,000"
+                dangerText="Risk exposure <= $10,000"
                 warningText="Risk exposure <= $100,000"
               />
 
