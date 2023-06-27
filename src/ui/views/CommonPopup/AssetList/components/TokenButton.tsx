735211d367205e3184ac29175e5f58a3f5e7fc4e
heisenberg <heisenberg-2077@outlook.com>
2023-06-27 17:21:24 +0800

    fix: customized token

--- src/ui/views/CommonPopup/AssetList/components/TokenButton.tsx
+++ src/ui/views/CommonPopup/AssetList/components/TokenButton.tsx
@@ -57,22 +57,21 @@ export const TokenButton: React.FC<Props> = ({
         <div className="text-black text-13 mb-[30px] text-center -m-8">
           The token in this list will not be added to total balance
         </div>
-        {len > 0 ? (
-          <div>
-            <TokenTable list={tokens} />
-          </div>
-        ) : (
-          <div className="space-y-24 text-13 text-center mt-[100px]">
-            <EmptySVG className="w-[52px] h-[52px] m-auto" />
-            <div className="text-gray-subTitle">{description}</div>
-            <div
-              onClick={handleClickLink}
-              className="text-blue-light underline cursor-pointer"
-            >
-              {linkText}
+        <TokenTable
+          list={tokens}
+          EmptyComponent={
+            <div className="space-y-24 text-13 text-center mt-[100px]">
+              <EmptySVG className="w-[52px] h-[52px] m-auto" />
+              <div className="text-gray-subTitle">{description}</div>
+              <div
+                onClick={handleClickLink}
+                className="text-blue-light underline cursor-pointer"
+              >
+                {linkText}
+              </div>
             </div>
-          </div>
-        )}
+          }
+        />
       </Popup>
     </div>
   );
