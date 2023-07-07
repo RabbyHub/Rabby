385247e47e2befe5374653da11b2c5960560b368
DMY <147dmy@gmail.com>
2023-07-07 18:24:18 +0800

    fix: style tuning

--- src/ui/views/Swap/Component/Main.tsx
+++ src/ui/views/Swap/Component/Main.tsx
@@ -380,7 +380,7 @@ export const Main = () => {
                   There is no fee and slippage for this trade
                 </div>
               ) : (
-                <div className="section text-13 text-gray-subTitle mt-12">
+                <div className="section text-13 leading-4 text-gray-subTitle mt-12">
                   <div className="subText flex flex-col gap-12">
                     <Slippage
                       displaySlippage={slippage}
