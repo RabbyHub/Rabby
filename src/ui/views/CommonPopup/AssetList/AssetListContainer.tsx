69ba9adbee561833ae5d3d7dee9a5717abe5a232
heisenberg <110591045+heisenberg-2077@users.noreply.github.com>
2023-07-13 10:32:23 +0800

    fix: show assets when blocked or custom tokens are found (#1520)
    * fix: show assets when blocked or custom tokens are found
    
    * fix: qrcode size

--- src/ui/views/CommonPopup/AssetList/AssetListContainer.tsx
+++ src/ui/views/CommonPopup/AssetList/AssetListContainer.tsx
@@ -42,6 +42,8 @@ export const AssetListContainer: React.FC<Props> = ({
     portfolios,
     tokens: tokenList,
     hasTokens,
+    blockedTokens,
+    customizeTokens,
   } = useQueryProjects(currentAccount?.address, false, visible);
   const [activeTab, setActiveTab] = React.useState<TokenTabEnum>(
     TokenTabEnum.List
@@ -50,7 +52,9 @@ export const AssetListContainer: React.FC<Props> = ({
     !isTokensLoading &&
     !tokenList.length &&
     !isPortfoliosLoading &&
-    !portfolios?.length;
+    !portfolios?.length &&
+    !blockedTokens?.length &&
+    !customizeTokens?.length;
 
   React.useEffect(() => {
     onEmptyAssets(isEmptyAssets);
@@ -126,6 +130,8 @@ export const AssetListContainer: React.FC<Props> = ({
               onFocusInput={handleFocusInput}
               isSearch={!!search}
               isNoResults={isNoResults}
+              blockedTokens={blockedTokens}
+              customizeTokens={customizeTokens}
             />
           )}
           {activeTab === TokenTabEnum.Summary && !search && (
