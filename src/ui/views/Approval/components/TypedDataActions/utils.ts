24545d42eb4314cca287642aff7df93a09c11d11
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2023-07-31 10:43:21 +0800

    fix: can sign after forbidden risk processed

--- src/ui/views/Approval/components/TypedDataActions/utils.ts
+++ src/ui/views/Approval/components/TypedDataActions/utils.ts
@@ -678,7 +678,7 @@ export const formatSecurityEngineCtx = ({
       },
     };
   }
-  if (actionData.contractCall && actionData.contractId && chain) {
+  if (actionData?.contractCall && actionData?.contractId && chain) {
     return {
       contractCall: {
         id: actionData.contractId,
