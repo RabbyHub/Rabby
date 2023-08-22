f5d911b6c2a8dcd819bf360c340efbee3142159e
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2023-08-22 14:14:32 +0800

    feat: change moonbeam gasLimit ratio to 2x

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -717,7 +717,9 @@ export const SWAP_FEE_PRECISION = 1e5;
 
 export const DEFAULT_GAS_LIMIT_RATIO = 1.5;
 
-export const SAFE_GAS_LIMIT_RATIO = {};
+export const SAFE_GAS_LIMIT_RATIO = {
+  '1284': 2,
+};
 export const GAS_TOP_UP_ADDRESS = '0x7559e1bbe06e94aeed8000d5671ed424397d25b5';
 export const GAS_TOP_UP_PAY_ADDRESS =
   '0x1f1f2bf8942861e6194fda1c0a9f13921c0cf117';
