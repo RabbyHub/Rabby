ff9317af46df86ac60cd27218093ffa9b43fb922
Max Korolev <nrjshka@gmail.com>
2023-08-23 16:39:16 +0100

    added: version update

--- changeLogs/index.ts
+++ changeLogs/index.ts
@@ -37,6 +37,7 @@ import version09213 from './09213.md';
 import version09216 from './09216.md';
 import version09217 from './09217.md';
 import version09218 from './09218.md';
+import version1000 from './1000.md';
 
 const version = process.env.release || '0';
 const versionMap = {
@@ -80,6 +81,7 @@ const versionMap = {
   '0.92.16': version09216,
   '0.92.17': version09217,
   '0.92.18': version09218,
+  '1.0.0': version1000,
 };
 export const getUpdateContent = () => {
   return versionMap[version];
