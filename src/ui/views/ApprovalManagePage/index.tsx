3782c18b095bbfdbb7738280afe76dce4cc7c74b
richardo2016x <richardo2016x@gmail.com>
2023-08-31 16:29:07 +0800

    feat: support display amount/balance info.

--- src/ui/views/ApprovalManagePage/index.tsx
+++ src/ui/views/ApprovalManagePage/index.tsx
@@ -722,7 +722,33 @@ function getColumnsForAsset({
 
         const spendValues = getSpenderApprovalAmount(spender);
 
-        return spendValues.displayText;
+        return (
+          <div className="text-14">
+            <Tooltip
+              overlayClassName="J-table__tooltip disable-ant-overwrite"
+              // Approved Amount
+              overlay={t(
+                'page.approvals.tableConfig.byAssets.columnCell.approvedAmount.tipApprovedAmount'
+              )}
+            >
+              <div className="text-r-neutral-title-1">
+                {spendValues.displayAmountText}
+              </div>
+            </Tooltip>
+
+            <Tooltip
+              overlayClassName="J-table__tooltip disable-ant-overwrite"
+              // My Balance
+              overlay={t(
+                'page.approvals.tableConfig.byAssets.columnCell.approvedAmount.tipMyBalance'
+              )}
+            >
+              <div className="text-r-neutral-foot mt-4">
+                {spendValues.displayBalanceText}
+              </div>
+            </Tooltip>
+          </div>
+        );
       },
       width: 160,
     },
