7ac38788d99c56d31095e9b4d506c05c57a9beca
Hodor <lost1q84@gmail.com>
2023-10-18 00:54:53 -0500

    feat: update page-provider (#1802)
    * chore
    
    * feat: update page-provider
    
    * feat: uuid
    
    * chore: remove console

--- src/content-script/index.ts
+++ src/content-script/index.ts
@@ -1,20 +1,26 @@
 import { Message } from '@/utils';
 import { nanoid } from 'nanoid';
 
+import { v4 as uuid } from 'uuid';
+
 const channelName = nanoid();
 
-// the script element with src won't execute immediately
-// use inline script element instead!
-const container = document.head || document.documentElement;
-const ele = document.createElement('script');
-// in prevent of webpack optimized code do some magic(e.g. double/sigle quote wrap),
-// seperate content assignment to two line
-// use AssetReplacePlugin to replace pageprovider content
-let content = `var channelName = '${channelName}';`;
-content += '#PAGEPROVIDER#';
-ele.textContent = content;
-container.insertBefore(ele, container.children[0]);
-container.removeChild(ele);
+const injectProviderScript = (isDefaultWallet: boolean) => {
+  // the script element with src won't execute immediately
+  // use inline script element instead!
+  const container = document.head || document.documentElement;
+  const ele = document.createElement('script');
+  // in prevent of webpack optimized code do some magic(e.g. double/sigle quote wrap),
+  // seperate content assignment to two line
+  // use AssetReplacePlugin to replace pageprovider content
+  let content = `var __rabby__channelName = '${channelName}';`;
+  content += `var __rabby__isDefaultWallet = ${isDefaultWallet};`;
+  content += `var __rabby__uuid = '${uuid()}';`;
+  content += '#PAGEPROVIDER#';
+  ele.textContent = content;
+  container.insertBefore(ele, container.children[0]);
+  container.removeChild(ele);
+};
 
 const { BroadcastChannelMessage, PortMessage } = Message;
 
@@ -31,3 +37,15 @@ document.addEventListener('beforeunload', () => {
   bcm.dispose();
   pm.dispose();
 });
+
+const getIsDefaultWallet = () => {
+  return pm.request({ method: 'isDefaultWallet' }) as Promise<boolean>;
+};
+
+getIsDefaultWallet()
+  .then((isDefaultWallet) => {
+    injectProviderScript(!!isDefaultWallet);
+  })
+  .catch((err) => {
+    injectProviderScript(true);
+  });
