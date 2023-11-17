c162e973c2d61f68fc8be8495c090ac487a3a466
richardo2016x <richardo2016x@gmail.com>
2023-11-17 12:48:03 +0800

    style: support dark mode for import-my-metamask

--- src/ui/views/ImportMyMetaMaskAccount/index.tsx
+++ src/ui/views/ImportMyMetaMaskAccount/index.tsx
@@ -5,8 +5,8 @@ import React from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router-dom';
 import IconMetamask from 'ui/assets/dashboard/icon-metamask.svg';
-import IconMnemonics from 'ui/assets/import/mnemonics.svg';
-import IconPrivatekey from 'ui/assets/import/privatekey.svg';
+import { ReactComponent as RcIconMnemonics } from 'ui/assets/import/mnemonics-currentcolor.svg';
+import { ReactComponent as RcIconPrivatekey } from 'ui/assets/import/privatekey-currentcolor.svg';
 import IconTinRightArrow from 'ui/assets/address/tiny-arrow-right.svg';
 
 import './style.less';
@@ -26,7 +26,7 @@ export const ImportMyMetaMaskAccount = () => {
           e.preventDefault();
           openInTab('https://rabby.io/metamask-export');
         }}
-        className="p-6 bg-[#EEF1FF] text-[#7084FF] text-12 font-medium relative top-8 rounded-[2px]"
+        className="p-6 bg-r-blue-light-1 text-r-blue-default text-12 font-medium relative top-8 rounded-[2px]"
       >
         Click to view tutorial
         <img
@@ -42,14 +42,14 @@ export const ImportMyMetaMaskAccount = () => {
   const importList = React.useMemo(
     () => [
       {
-        icon: IconMnemonics,
+        icon: RcIconMnemonics,
         content: t('page.newAddress.importSeedPhrase'),
         onClick: () => {
           openInternalPageInTab('import/mnemonics');
         },
       },
       {
-        icon: IconPrivatekey,
+        icon: RcIconPrivatekey,
         content: t('page.newAddress.importPrivateKey'),
         onClick: () => {
           history.push('/import/key');
@@ -63,8 +63,8 @@ export const ImportMyMetaMaskAccount = () => {
       <BlueHeader className="mx-[-20px]">
         {t('page.newAddress.importMyMetamaskAccount')}
       </BlueHeader>
-      <div className="rabby-container">
-        <div className="relative bg-white mt-[12px] rounded-[6px] px-[12px] py-[12px] mb-[12px]">
+      <div className="rabby-container text-r-neutral-title-1">
+        <div className="relative bg-r-neutral-card-1 mt-[12px] rounded-[6px] px-[12px] py-[12px] mb-[12px]">
           <div className="metamask-shadow" />
 
           <div className="flex items-center">
@@ -73,15 +73,15 @@ export const ImportMyMetaMaskAccount = () => {
               className="w-[32px] h-[32px]"
               alt="MetaMask"
             />
-            <div className="pl-[12px] text-[16px] font-medium text-[#192945]">
+            <div className="pl-[12px] text-[16px] font-medium text-r-neutral-title-1">
               {t('page.newAddress.metamask.how')}
             </div>
           </div>
-          <div className="relative rounded border border-[#7084FF] mt-14 mb-16">
-            <div className="absolute left-[9px] top-[-8px] bg-white text-[#7084FF] text-12 font-medium pl-2 pr-4">
+          <div className="relative rounded border border-rabby-blue-default mt-14 mb-16">
+            <div className="absolute left-[9px] top-[-8px] bg-r-neutral-card-1 text-r-blue-default text-12 font-medium pl-2 pr-4">
               {t('page.newAddress.metamask.tips')}
             </div>
-            <div className="p-8 text-[#7084FF] text-12 font-normal leading-[16px]">
+            <div className="p-8 text-r-blue-default text-12 font-normal leading-[16px]">
               {t('page.newAddress.metamask.tipsDesc')}
             </div>
           </div>
@@ -92,7 +92,7 @@ export const ImportMyMetaMaskAccount = () => {
                 <Timeline.Item
                   color="transparent"
                   dot={
-                    <span className="text-13 font-medium text-[#192945]">
+                    <span className="text-13 font-medium text-r-neutral-title-1">
                       {t('page.newAddress.metamask.step')}
                       {i + 1}:
                     </span>
@@ -100,7 +100,7 @@ export const ImportMyMetaMaskAccount = () => {
                 >
                   <div
                     className={clsx(
-                      'text-13 font-medium text-[#192945] leading-[18px]',
+                      'text-13 font-medium text-r-neutral-title-1 leading-[18px]',
                       i === 0 && 'ml-[-2px]'
                     )}
                   >
@@ -112,26 +112,27 @@ export const ImportMyMetaMaskAccount = () => {
           </div>
         </div>
 
-        <div className="bg-white rounded-[6px] ">
-          <div className="border-b border-gray-divider py-[15px] px-[19px]">
-            <div className="text-15 font-medium text-[#192945]">
+        <div className="bg-r-neutral-card-1 rounded-[6px] ">
+          <div className="border-b border-rabby-neutral-line py-[15px] px-[19px]">
+            <div className="text-15 font-medium text-r-neutral-title-1">
               {t('page.newAddress.metamask.importSeedPhrase')}
             </div>
 
-            <div className="mt-[3px] text-12 text-[#3E495E]">
+            <div className="mt-[3px] text-12 text-r-neutral-body">
               {t('page.newAddress.metamask.importSeedPhraseTips')}
             </div>
           </div>
 
-          {importList.map((e) => (
+          {importList.map((e, idx) => (
             <Item
-              key={e.icon}
+              bgColor="transparent"
+              key={`${e.content}-${idx}`}
               leftIcon={e.icon}
-              leftIconClassName="icon"
+              leftIconClassName="icon text-r-neutral-body"
               onClick={e.onClick}
               py={15}
             >
-              <div className="pl-[12px] text-13 font-medium text-[#192945]">
+              <div className="pl-[12px] text-13 font-medium text-r-neutral-title-1">
                 {e.content}
               </div>
             </Item>
