6721dfe2214cc65fd1f49fce9f799da625265745
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2023-12-08 17:01:40 +0800

    feat: + changelog

--- changeLogs/index.ts
+++ changeLogs/index.ts
@@ -55,6 +55,7 @@ import version09235 from './09235.md';
 import version09236 from './09236.md';
 import version09237 from './09237.md';
 import version09238 from './09238.md';
+import version09239 from './09239.md';
 
 const version = process.env.release || '0';
 const versionMap = {
@@ -116,6 +117,7 @@ const versionMap = {
   '0.92.36': version09236,
   '0.92.37': version09237,
   '0.92.38': version09238,
+  '0.92.39': version09239,
 };
 export const getUpdateContent = () => {
   return versionMap[version];
