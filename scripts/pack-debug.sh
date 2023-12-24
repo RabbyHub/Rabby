c4cee9d94a620f649275fd8cf595afc03f2f78f2
richardo2016x <richardo2016x@gmail.com>
2023-12-24 18:31:27 +0800

    build: add script ./pack-debug.sh

---
+++ scripts/pack-debug.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+script_dir="$( cd "$( dirname "$0"  )" && pwd  )"
+project_dir=$(dirname "$script_dir")
+systype=$(uname -s)
+
+cd $project_dir;
+app_ver=$(node -p "require('./package.json').version")
+
+pack_dist() {
+  rm -rf ./tmp/ && mkdir -p ./tmp/ && \
+  zip -r ./tmp/Rabby_v${app_ver}_debug.zip ./dist
+}
+
+build() {
+  yarn build:debug;
+}
+
+if [ "$1" = "--pack-only" ]; then
+  pack_dist;
+else
+  build && pack_dist;
+fi
+
+case $systype in
+  "Darwin")
+    open ./tmp/
+    ;;
+  MSYS_NT*|MINGW*)
+    start "" .\\tmp
+    ;;
+esac
