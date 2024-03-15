e36569b74c2d8ee8370c6c4f6569c4a92dafaed7
DMY <147dmy@gmail.com>
2024-03-15 17:54:05 +0800

    fix: style

--- src/ui/component/Item/index.tsx
+++ src/ui/component/Item/index.tsx
@@ -197,7 +197,10 @@ export const BlueHeader = ({
     <>
       <BlueHeaderWrapper {...rest}>
         {showBackIcon && (
-          <div className={clsx('back', leftIconClassName)} onClick={goBack}>
+          <div
+            className={clsx('back top-1/2 -translate-y-2/4', leftIconClassName)}
+            onClick={goBack}
+          >
             <img src={ArrowLeftWhiteBack} />
           </div>
         )}
