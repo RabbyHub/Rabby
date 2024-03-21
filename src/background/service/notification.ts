7ea663717cd58640797c8b86c758cf3be0b08783
heisenberg <heisenberg-2077@outlook.com>
2024-03-21 22:06:59 +0800

    fix: setBadgeText

--- src/background/service/notification.ts
+++ src/background/service/notification.ts
@@ -96,7 +96,7 @@ class NotificationService extends Events {
 
     if (val.length <= 0) {
       action.setBadgeText({
-        text: '',
+        text: isManifestV3 ? '' : null,
       });
     } else {
       action.setBadgeText({
