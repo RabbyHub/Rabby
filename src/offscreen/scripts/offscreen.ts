34244faf6904ac89916b2be929cc115c09534521
heisenberg <110591045+heisenberg-2077@users.noreply.github.com>
2024-03-26 18:29:28 +0800

    feat: use @trezor/connect-webextension (#2136)

--- src/offscreen/scripts/offscreen.ts
+++ src/offscreen/scripts/offscreen.ts
@@ -2,11 +2,9 @@ import { initBitBox02 } from './bitbox02';
 import { initImKey } from './imkey';
 import initLattice from './lattice';
 import { initOneKey } from './onekey';
-import { initTrezor } from './trezor';
 
 initImKey();
 initOneKey();
-initTrezor();
 initBitBox02();
 initLattice();
 
