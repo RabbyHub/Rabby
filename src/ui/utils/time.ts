83a14ed6b35fbb760830d7f0c32e10d0e90f7db4
vvvvvv1vvvvvv <86296331+vvvvvv1vvvvvv@users.noreply.github.com>
2024-04-19 16:08:15 +0800

    fix: use forever for a big expire time (#2219)
    * fix: use forever for a big expire time
    
    * feat: + changelog

--- src/ui/utils/time.ts
+++ src/ui/utils/time.ts
@@ -30,10 +30,13 @@ export function getTimeSpan(times: number) {
     times = 0;
   }
   const int = Math.floor(times);
-  const d = parseInt(int / 60 / 60 / 24 + '');
-  const h = parseInt(((int / 60 / 60) % 24) + '');
-  const m = parseInt(((int / 60) % 60) + '');
-  const s = parseInt((int % 60) + '');
+  let d = Math.floor(int / 60 / 60 / 24);
+  const h = Math.floor((int / 60 / 60) % 24);
+  const m = Math.floor((int / 60) % 60);
+  const s = Math.floor(int % 60);
+  if (d >= 365000) {
+    d = 365000;
+  }
   return {
     d,
     h,
