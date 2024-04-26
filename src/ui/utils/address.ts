c9deee455756d2adb6a02c73ea47eeb5d38d1adf
vvvvvv1vvvvvv <86296331+vvvvvv1vvvvvv@users.noreply.github.com>
2024-04-26 10:47:08 +0800

    fix: typedata address bypass (#2238)

--- src/ui/utils/address.ts
+++ src/ui/utils/address.ts
@@ -9,3 +9,24 @@ export const enum AddressType {
   CONTRACT = 'CONTRACT',
   UNKNOWN = 'UNKNOWN',
 }
+
+export type Hex = `0x${string}`;
+
+export function add0x(hexadecimal: string): Hex {
+  if (hexadecimal.startsWith('0x')) {
+    return hexadecimal as Hex;
+  }
+
+  if (hexadecimal.startsWith('0X')) {
+    return `0x${hexadecimal.substring(2)}`;
+  }
+
+  return `0x${hexadecimal}`;
+}
+
+export function isStrictHexString(value: unknown): value is Hex {
+  if (typeof value === 'string') {
+    return /^0x[0-9a-f]+$/iu.test(value);
+  }
+  return false;
+}
