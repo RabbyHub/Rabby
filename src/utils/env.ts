d6a2c17681d9d0dbda4c50845fd9a78d16181111
richardo2016x <richardo2016x@gmail.com>
2024-04-29 12:49:42 +0800

    fix: env var.

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -15,7 +15,7 @@ export const getSentryEnv = () => {
 };
 
 export const appIsDebugPkg =
-  process.env.DEBUG === 'true' && process.env.BUILD_ENV === 'PRO';
+  `${process.env.DEBUG}` === 'true' && process.env.BUILD_ENV === 'PRO';
 
 export const appIsProd = process.env.NODE_ENV === 'production';
 export const appIsDev = !appIsProd;
