4c038cead67ddef898a746038c179b5558175d09
heisenberg <heisenberg-2077@outlook.com>
2024-05-22 16:14:53 +0800

    fix: ellipsis

--- src/ui/utils/address.ts
+++ src/ui/utils/address.ts
@@ -1,5 +1,5 @@
 export const ellipsis = (text: string) => {
-  return text?.replace(/^(.{6})(.*)(.{4})$/, '$1...$3');
+  return text.toString().replace(/^(.{6})(.*)(.{4})$/, '$1...$3');
 };
 
 export const ellipsisAddress = ellipsis;
