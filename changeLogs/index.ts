aa85e878f36d5219812e2ea68b9f95b1fa509989
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2024-06-14 18:17:21 +0800

    feat: + changelog

--- changeLogs/index.ts
+++ changeLogs/index.ts
@@ -80,6 +80,7 @@ import version09271 from './09271.md';
 import version09272 from './09272.md';
 import version09273 from './09273.md';
 import version09274 from './09274.md';
+import version09276 from './09276.md';
 
 const version = process.env.release || '0';
 const versionMap = {
@@ -166,6 +167,7 @@ const versionMap = {
   '0.92.72': version09272,
   '0.92.73': version09273,
   '0.92.74': version09274,
+  '0.92.76': version09276,
 };
 export const getUpdateContent = () => {
   return versionMap[version];
