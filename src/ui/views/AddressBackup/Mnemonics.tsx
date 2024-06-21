fac82045bba33a2ded9f4ed186035a73ab138519
Hodor <lost1q84@gmail.com>
2024-06-21 16:37:14 +0800

    feat: support backup seed phrase use qr code (#2346)

--- src/ui/views/AddressBackup/Mnemonics.tsx
+++ src/ui/views/AddressBackup/Mnemonics.tsx
@@ -15,6 +15,9 @@ import { useHistory, useLocation } from 'react-router-dom';
 import IconBack from 'ui/assets/back.svg';
 import ThemeIcon from '@/ui/component/ThemeMode/ThemeIcon';
 import { Slip39TextareaContainer } from './Slip39TextAreaContainer';
+import { Popup } from '@/ui/component';
+import QRCode from 'qrcode.react';
+import { ReactComponent as RcIconQrCode } from 'ui/assets/qrcode-cc.svg';
 
 const AddressBackup = () => {
   const wallet = useWallet();
@@ -38,6 +41,29 @@ const AddressBackup = () => {
       });
     });
   }, [data]);
+
+  const handleShowQrCode = () => {
+    Popup.open({
+      title: t('page.backupSeedPhrase.qrCodePopupTitle'),
+      height: 476,
+      closable: true,
+      content: (
+        <div>
+          <div className="flex items-start gap-8 px-[12px] py-[10px] rounded-[4px] bg-r-red-light text-r-red-default mb-[20px]">
+            <InfoCircleOutlined className="rotate-180" />
+            <div className="text-[14px] leading-[18px] ">
+              {t('page.backupSeedPhrase.qrCodePopupTips')}
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <div className="p-[12px] rounded-[16px] border-rabby-neutral-line border-[1px] bg-r-neutral-card-1">
+              <QRCode value={data} size={240} />
+            </div>
+          </div>
+        </div>
+      ),
+    });
+  };
 
   const isSlip39 = React.useMemo(() => {
     return data.split('\n').length > 1;
@@ -66,7 +92,7 @@ const AddressBackup = () => {
         {t('page.backupSeedPhrase.title')}
       </header>
       <div className="alert mb-[34px]">
-        <InfoCircleOutlined />
+        <InfoCircleOutlined className="rotate-180" />
         {t('page.backupSeedPhrase.alert')}
       </div>
       <div className="mb-[74px]">
@@ -96,18 +122,33 @@ const AddressBackup = () => {
             )}
           </div>
         </div>
-        <div
-          onClick={onCopyMnemonics}
-          className={clsx(
-            'copy text-r-neutral-foot',
-            masked ? 'invisible' : 'visible'
-          )}
-        >
-          <ThemeIcon
-            src={RcIconCopyCC}
-            className="text-r-neutral-foot w-[16px] h-[16px]"
-          />
-          {t('page.backupSeedPhrase.copySeedPhrase')}
+        <div className="flex items-center gap-[24px] justify-center">
+          <div
+            onClick={handleShowQrCode}
+            className={clsx(
+              'copy text-r-neutral-foot',
+              masked ? 'invisible' : 'visible'
+            )}
+          >
+            <ThemeIcon
+              src={RcIconQrCode}
+              className="text-r-neutral-foot w-[16px] h-[16px]"
+            />
+            {t('page.backupSeedPhrase.showQrCode')}
+          </div>
+          <div
+            onClick={onCopyMnemonics}
+            className={clsx(
+              'copy text-r-neutral-foot',
+              masked ? 'invisible' : 'visible'
+            )}
+          >
+            <ThemeIcon
+              src={RcIconCopyCC}
+              className="text-r-neutral-foot w-[16px] h-[16px]"
+            />
+            {t('page.backupSeedPhrase.copySeedPhrase')}
+          </div>
         </div>
       </div>
       <div className="footer pb-[20px]">
