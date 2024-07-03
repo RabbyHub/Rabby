c372357e3336edeea4223a7f4dc3d789007bad89
fhasghrge <1969533391@qq.com>
2024-07-03 17:22:37 +0800

    feat: mint nft ui

---
+++ src/ui/views/Ecology/dbk-chain/MintNFT.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import MintNFTIcon from 'ui/assets/ecology/dbk-genesis-nft.png';
+import { DbkButton } from './components/DbkButton';
+
+const MintNFT: React.FC = () => {
+  return (
+    <div className="p-[20px] ">
+      <div className="bg-r-neutral-card1 flex flex-col items-center rounded-[8px]">
+        <div className="flex flex-col pt-[32px] pb-[35px] items-center text-center">
+          <img className="block w-[200px] h-[200px]" src={MintNFTIcon} alt="" />
+          <div className="mt-[20px] text-gradient text-[24px] leading-[28.8px] font-[700]">
+            DBK Genesis
+          </div>
+        </div>
+        <div className="flex flex-row mt-[20px] justify-center text-center ">
+          <div className="w-[164px] h-[64px] flex flex-col">
+            <div className="text-r-green-default text-[20px] leading-[24px] font-[700]">
+              3990
+            </div>
+            <div className="text-r-neutral-foot text-[13px] mt-[4px]">
+              Minted
+            </div>
+          </div>
+          <div className="w-[164px] h-[64px] flex flex-col">
+            <div className="text-r-green-default text-[20px] leading-[24px] font-[700]">
+              0
+            </div>
+            <div className="text-r-neutral-foot text-[13px] mt-[4px]">
+              My Balance
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer
+        className={`absolute bottom-0 left-0 w-full px-[20px] py-[18px] 
+          bg-r-neutral-bg1 border-t-[0.5px] border-t-r-neutral-line`}
+      >
+        <DbkButton
+          style={{
+            width: '100%',
+            height: '44px',
+          }}
+        >
+          Mint
+        </DbkButton>
+      </footer>
+    </div>
+  );
+};
+
+export default MintNFT;
