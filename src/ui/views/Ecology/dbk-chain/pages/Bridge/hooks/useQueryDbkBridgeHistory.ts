2554d0027264661a17b2c2e90bb7663952ec899e
Hodor <lost1q84@gmail.com>
2024-07-12 16:09:55 +0800

    fix: page

--- src/ui/views/Ecology/dbk-chain/pages/Bridge/hooks/useQueryDbkBridgeHistory.ts
+++ src/ui/views/Ecology/dbk-chain/pages/Bridge/hooks/useQueryDbkBridgeHistory.ts
@@ -2,23 +2,27 @@ import { useCurrentAccount } from '@/ui/hooks/backgroundState/useAccount';
 import { useWallet } from '@/ui/utils';
 import { useInfiniteScroll, useRequest } from 'ahooks';
 
+const PageSize = 20;
+
 export const useQueryDbkBridgeHistory = () => {
   const wallet = useWallet();
   const account = useCurrentAccount();
   return useInfiniteScroll(
-    async () => {
+    async (prev) => {
       if (!account?.address) {
         return {
           list: [],
           page: {
             total: 0,
             start: 0,
-            limit: 0,
+            limit: PageSize,
           },
         };
       }
       const data = await wallet.openapi.getDbkBridgeHistoryList({
         user_addr: account.address,
+        start: prev ? prev.page.start + prev.page.limit : 0,
+        limit: PageSize,
       });
       return {
         list: data.data || [],
