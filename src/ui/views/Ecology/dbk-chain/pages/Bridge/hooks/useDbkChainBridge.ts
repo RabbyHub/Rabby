690edfa12ea2411f30536a890af9c81db255af4a
Hodor <lost1q84@gmail.com>
2024-07-16 16:49:07 +0800

    fix: gasLimit

--- src/ui/views/Ecology/dbk-chain/pages/Bridge/hooks/useDbkChainBridge.ts
+++ src/ui/views/Ecology/dbk-chain/pages/Bridge/hooks/useDbkChainBridge.ts
@@ -40,55 +40,17 @@ export const useDbkChainBridge = ({
   const [payAmount, setPayAmount] = useState('');
   const account = useCurrentAccount();
 
-  const buildInitiateWithdrawal = useMemoizedFn(async () => {
-    try {
-      const args = await clientL1.buildInitiateWithdrawal({
-        account: account!.address as any,
-        to: account!.address as any,
-        value: parseEther(payAmount),
-      });
-      return args;
-    } catch (e) {
-      console.error(e);
-      return {
-        account: account!.address as any,
-        request: {
-          data: undefined,
-          // https://github.com/superbridgeapp/superbridge-app/blob/main/apps/bridge/hooks/use-transaction-args/withdraw-args/use-optimism-withdraw-args.ts#L58
-          gas: BigInt(200_000),
-          to: account!.address as any,
-          value: parseEther(payAmount),
-        },
-      };
-    }
-  });
-
-  const buildDepositTransactionGas = useMemoizedFn(async () => {
-    try {
-      const args = await clientL2.buildDepositTransaction({
-        account: (account!.address as unknown) as `0x${string}`,
-        mint: parseEther(payAmount),
-        to: (account!.address as unknown) as `0x${string}`,
-      });
-      return args.request.gas;
-    } catch (e) {
-      console.error(e);
-      return BigInt(200_000);
-    }
-  });
-
   const _handleDeposit = useMemoizedFn(async () => {
     if (!account?.address) {
       return;
     }
     try {
-      const l2Gas = await buildDepositTransactionGas();
       const contract = getContract({
         abi: l1StandardBridgeABI,
         address: DBK_CHAIN_BRIDGE_CONTRACT,
         client: clientL1,
       });
-      const hash = await contract.write.depositETH([Number(l2Gas), '0x'], {
+      const hash = await contract.write.depositETH([200_000, '0x'], {
         account: account.address as `0x${string}`,
         value: parseEther(payAmount),
         gas: undefined,
@@ -116,9 +78,14 @@ export const useDbkChainBridge = ({
       return;
     }
     try {
-      const args = await buildInitiateWithdrawal();
       const hash = await clientL2.initiateWithdrawal({
-        ...args,
+        account: account.address as `0x${string}`,
+        request: {
+          // https://github.com/superbridgeapp/superbridge-app/blob/main/apps/bridge/hooks/use-transaction-args/withdraw-args/use-optimism-withdraw-args.ts#L58
+          gas: BigInt(200_000),
+          to: account.address as `0x${string}`,
+          value: parseEther(payAmount),
+        },
         gas: null,
       });
       if (hash) {
