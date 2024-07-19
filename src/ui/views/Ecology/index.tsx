05085810d20f14dac2c57739fa98bdad54360572
Hodor <lost1q84@gmail.com>
2024-07-19 11:38:16 +0800

    chore

--- src/ui/views/Ecology/index.tsx
+++ src/ui/views/Ecology/index.tsx
@@ -3,8 +3,6 @@ import { useTranslation } from 'react-i18next';
 
 import { useParams } from 'react-router-dom';
 import { DbkChainEntry } from './dbk-chain/Entry';
-import { EcologyNoticeModal } from '@/ui/component/Ecology/EcologyNoticeModal';
-import { useRabbyDispatch, useRabbySelector } from '@/ui/store';
 
 export const Ecology = () => {
   const { t } = useTranslation();
