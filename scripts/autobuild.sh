74ede2c1d9229a1876c4243e145c8a942b9ac491
richardo2016x <richardo2016x@gmail.com>
2024-07-31 16:33:02 +0800

    build: fix.

--- scripts/autobuild.sh
+++ scripts/autobuild.sh
@@ -45,7 +45,7 @@ if [ -z $NO_UPLOAD ]; then
     aws s3 cp $QUIET_PARASM $project_dir/tmp/ s3://$RABBY_BUILD_BUCKET/rabby/autobuild/RabbyDebug-$CURRENT_TIME --recursive --exclude="*" --include "*.zip" --acl public-read
     echo "[pack] uploaded. DOWNLOAD_URL is $DOWNLOAD_URL";
 
-    if [ "$notify_lark" === "true" ]; then
+    if [ ! -z $notify_lark ]; then
         echo "[pack] update latest link...";
         node ./scripts/notify-lark.js "$DOWNLOAD_URL"
     else
