bc02a58c502519c57e820c61c0a4475aab4637e5
Hodor <lost1q84@gmail.com>
2024-08-15 15:21:49 +0800

    chore

--- src/background/controller/provider/rpcFlow.ts
+++ src/background/controller/provider/rpcFlow.ts
@@ -245,7 +245,6 @@ const flowContext = flow
           )
             .then((result) => {
               if (isSignApproval(approvalType)) {
-                console.log('emit sign finished');
                 eventBus.emit(EVENTS.broadcastToUI, {
                   method: EVENTS.SIGN_FINISHED,
                   params: {
