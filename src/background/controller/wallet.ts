9a685722c62dbbd379aecc401bbc535e4630f82b
Hodor <lost1q84@gmail.com>
2024-08-16 14:38:20 +0800

    fix: safe sign (#2474)

--- src/background/controller/wallet.ts
+++ src/background/controller/wallet.ts
@@ -1845,14 +1845,15 @@ export class WalletController extends BaseController {
   ) => {
     const keyring: GnosisKeyring = this._getKeyringByType(KEYRING_CLASS.GNOSIS);
     if (keyring) {
-      buildinProvider.currentProvider.currentAccount = account.address;
-      buildinProvider.currentProvider.currentAccountType = account.type;
-      buildinProvider.currentProvider.currentAccountBrand = account.brandName;
-      buildinProvider.currentProvider.chainId = networkId;
+      const currentProvider = new EthereumProvider();
+      currentProvider.currentAccount = account.address;
+      currentProvider.currentAccountType = account.type;
+      currentProvider.currentAccountBrand = account.brandName;
+      currentProvider.chainId = networkId;
       await keyring.buildTransaction(
         safeAddress,
         tx,
-        new ethers.providers.Web3Provider(buildinProvider.currentProvider),
+        new ethers.providers.Web3Provider(currentProvider),
         version,
         networkId
       );
