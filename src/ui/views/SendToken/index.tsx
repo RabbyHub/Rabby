28521cf54ef8156c1bb5665e977876ba1541c300
richardo2016x <richardo2016x@gmail.com>
2024-09-04 16:39:01 +0800

    fix: estimate logic

--- src/ui/views/SendToken/index.tsx
+++ src/ui/views/SendToken/index.tsx
@@ -818,9 +818,10 @@ const SendToken = () => {
         console.error(err);
       }
 
-      const gasUsed = lastestChainItem.isTestnet
-        ? new BigNumber(_gasUsed).multipliedBy(1.5).integerValue().toNumber()
-        : _gasUsed;
+      const gasUsed = new BigNumber(_gasUsed)
+        .multipliedBy(1.5)
+        .integerValue()
+        .toNumber();
 
       result.gasNumber = Number(gasUsed);
       result.gasNumHex =
