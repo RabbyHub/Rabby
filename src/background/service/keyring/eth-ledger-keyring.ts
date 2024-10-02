e65f7d5266f113f8df617520c2168b4c19c79f22
Louis Aussedat <louis.aussedat@ledger.fr>
2024-10-02 15:00:08 +0200

    enable EIP712 clear sign

--- src/background/service/keyring/eth-ledger-keyring.ts
+++ src/background/service/keyring/eth-ledger-keyring.ts
@@ -406,25 +406,6 @@ class LedgerBridgeKeyring {
         );
       }
 
-      const {
-        domain,
-        types,
-        primaryType,
-        message,
-      } = sigUtil.TypedDataUtils.sanitizeData(data);
-      const domainSeparatorHex = sigUtil.TypedDataUtils.hashStruct(
-        'EIP712Domain',
-        domain,
-        types,
-        isV4
-      ).toString('hex');
-      const hashStructMessageHex = sigUtil.TypedDataUtils.hashStruct(
-        primaryType as string,
-        message,
-        types,
-        isV4
-      ).toString('hex');
-
       const hdPath = await this.unlockAccountByAddress(withAccount);
       try {
         await this.makeApp(true);
@@ -436,30 +417,38 @@ class LedgerBridgeKeyring {
         };
 
         // https://github.com/LedgerHQ/ledger-live/blob/5bae039273beeeb02d8640d778fd7bf5f7fd3776/libs/coin-evm/src/hw-signMessage.ts#L68C7-L79C10
-        // try {
-        //   res = await this.app!.signEIP712Message(hdPath, data);
-        // } catch (e) {
-        //   if (
-        //     e instanceof Error &&
-        //     'statusText' in e &&
-        //     (e as any).statusText === 'INS_NOT_SUPPORTED'
-        //   ) {
-        //     res = await this.app!.signEIP712HashedMessage(
-        //       hdPath,
-        //       domainSeparatorHex,
-        //       hashStructMessageHex
-        //     );
-        //   } else {
-        //     throw e;
-        //   }
-        // }
+        try {
+          res = await this.app!.signEIP712Message(hdPath, data);
+        } catch (e) {
+          const shouldFallbackOnHashedMethod =
+            'statusText' in e && e.statusText === 'INS_NOT_SUPPORTED';
+          if (!shouldFallbackOnHashedMethod) throw e;
 
-        // eslint-disable-next-line prefer-const
-        res = await this.app!.signEIP712HashedMessage(
-          hdPath,
-          domainSeparatorHex,
-          hashStructMessageHex
-        );
+          const {
+            domain,
+            types,
+            primaryType,
+            message,
+          } = sigUtil.TypedDataUtils.sanitizeData(data);
+          const domainSeparatorHex = sigUtil.TypedDataUtils.hashStruct(
+            'EIP712Domain',
+            domain,
+            types,
+            isV4
+          ).toString('hex');
+          const hashStructMessageHex = sigUtil.TypedDataUtils.hashStruct(
+            primaryType as string,
+            message,
+            types,
+            isV4
+          ).toString('hex');
+
+          res = await this.app!.signEIP712HashedMessage(
+            hdPath,
+            domainSeparatorHex,
+            hashStructMessageHex
+          );
+        }
 
         let v = res.v.toString(16);
         if (v.length < 2) {
