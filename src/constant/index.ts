85b523f9f5909283b843d2280670d8ed9972f447
hz002 <zs4733525@gmail.com>
2024-10-24 12:24:53 +0800

    fix: hover icon

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -560,6 +560,8 @@ export type IWalletBrandContent = {
   brand: WALLET_BRAND_TYPES;
   icon: string;
   lightIcon: string;
+  // Icon showed out of collapse
+  leftIcon?: string;
   image: string;
   rcSvg: Exclude<ThemeIconType, string>;
   /**
@@ -743,6 +745,7 @@ export const WALLET_BRAND_CONTENT: {
     brand: WALLET_BRAND_TYPES.LEDGER,
     icon: LogoLedgerWhite,
     lightIcon: LogoLedgerWhite,
+    leftIcon: LogoLedgerDark,
     image: IS_FIREFOX ? LogoLedgerDisable : LogoLedgerDark,
     rcSvg: IS_FIREFOX ? RcLogoLedgerDisable : RcLogoLedgerDark,
     maybeSvg: LogoLedgerDark,
