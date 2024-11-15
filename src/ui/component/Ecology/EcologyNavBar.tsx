6265d3cffaa56299af27e03895598dbc14c651df
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2024-11-15 15:00:02 +0800

    feat: eco support non-intergrate chains

--- src/ui/component/Ecology/EcologyNavBar.tsx
+++ src/ui/component/Ecology/EcologyNavBar.tsx
@@ -1,23 +1,22 @@
-import { findChain } from '@/utils/chain';
 import clsx from 'clsx';
 import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router-dom';
 import { ReactComponent as RcIconBack } from 'ui/assets/icon-back-cc.svg';
+import { EcoChains } from 'ui/views/Ecology/constants';
 
 interface Props {
   className?: string;
   style?: React.CSSProperties;
   chainId: number;
 }
+
+const findChain = (id: number) => {
+  return EcoChains.find((chain) => chain.id === id);
+};
+
 export const EcologyNavBar = ({ className, style, chainId }: Props) => {
-  const chain = useMemo(
-    () =>
-      findChain({
-        id: chainId,
-      }),
-    [chainId]
-  );
+  const chain = useMemo(() => findChain(chainId), [chainId]);
   const history = useHistory();
 
   const { t } = useTranslation();
