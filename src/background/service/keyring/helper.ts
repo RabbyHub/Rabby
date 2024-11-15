5ace735a82565e31688b701c745c225a5428596b
heisenberg <heisenberg-2077@outlook.com>
2024-11-15 10:27:12 +0800

    Update helper.ts

--- src/background/service/keyring/helper.ts
+++ src/background/service/keyring/helper.ts
@@ -2,13 +2,6 @@ import { EVENTS } from '@/constant';
 import eventBus from '@/eventBus';
 import * as Sentry from '@sentry/browser';
 
-export const throwError = (error, method = EVENTS.COMMON_HARDWARE.REJECTED) => {
-  eventBus.emit(EVENTS.broadcastToUI, {
-    method,
-    params: error,
-  });
-};
-
 export enum LedgerHDPathType {
   LedgerLive = 'LedgerLive',
   Legacy = 'Legacy',
