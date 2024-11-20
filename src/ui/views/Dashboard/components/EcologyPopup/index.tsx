c6f9a666aa492fe8d9e1f895700ee587fb4d5bf8
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2024-11-20 16:39:52 +0800

    feat: support non-intergrate chains for eco popup

--- src/ui/views/Dashboard/components/EcologyPopup/index.tsx
+++ src/ui/views/Dashboard/components/EcologyPopup/index.tsx
@@ -1,13 +1,11 @@
-import { DBK_CHAIN_ID } from '@/constant';
 import { Popup } from '@/ui/component';
 import ThemeIcon from '@/ui/component/ThemeMode/ThemeIcon';
-import { findChain } from '@/utils/chain';
-import { Chain } from '@debank/common';
 import clsx from 'clsx';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router-dom';
 import { ReactComponent as RcIconArrowRight } from 'ui/assets/dashboard/settings/icon-right-arrow.svg';
+import { EcoChains } from 'ui/views/Ecology/constants';
 
 interface Props {
   visible?: boolean;
@@ -16,13 +14,6 @@ interface Props {
 export const EcologyPopup = ({ visible, onClose }: Props) => {
   const { t } = useTranslation();
   const history = useHistory();
-  const chainList = [DBK_CHAIN_ID]
-    .map((chainId) => {
-      return findChain({
-        id: chainId,
-      });
-    })
-    .filter((chain): chain is Chain => !!chain);
 
   return (
     <Popup
@@ -32,7 +23,7 @@ export const EcologyPopup = ({ visible, onClose }: Props) => {
       push={false}
       title={t('page.dashboard.echologyPopup.title')}
     >
-      {chainList.map((item) => {
+      {EcoChains.map((item) => {
         return (
           <div
             key={item.id}
