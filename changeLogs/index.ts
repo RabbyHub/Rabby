66f57bdad52e8cdc5e6957417e4a85596256a68b
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2025-03-13 19:02:32 +0800

    feat: + changelog

--- changeLogs/index.ts
+++ changeLogs/index.ts
@@ -1,8 +1,10 @@
 import version09318 from './09318.md';
+import version09319 from './09319.md';
 
 const version = process.env.release || '0';
 const versionMap = {
   '0.93.18': version09318,
+  '0.93.19': version09319,
 };
 export const getUpdateContent = () => {
   return versionMap[version];
