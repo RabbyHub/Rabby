b2b681bd6ccfef5af9e809c3da3f5d8058abe8d5
heisenberg <heisenberg-2077@outlook.com>
2025-03-18 14:42:10 +0800

    wip

--- src/background/service/keyring/eth-cobo-argus-keyring.ts
+++ src/background/service/keyring/eth-cobo-argus-keyring.ts
@@ -1,6 +1,6 @@
 import { addHexPrefix } from '@ethereumjs/util';
 import EventEmitter from 'events';
-import { isAddress } from 'web3-utils';
+import { isAddress } from 'viem';
 
 export const keyringType = 'CoboArgus';
 
