4efe7ec401b2520c7de7184abb3081edcd393e32
hz002 <zs4733525@gmail.com>
2025-05-28 21:07:51 +0800

    feat: style

--- src/ui/views/CreateMnemonics/DisplayMnemonic.tsx
+++ src/ui/views/CreateMnemonics/DisplayMnemonic.tsx
@@ -1,61 +1,16 @@
 import React, { useEffect } from 'react';
-import styled from 'styled-components';
-import { InfoCircleOutlined } from '@ant-design/icons';
 import WordsMatrix from '@/ui/component/WordsMatrix';
 import clsx from 'clsx';
 import { connectStore, useRabbyDispatch, useRabbySelector } from 'ui/store';
-import { styid } from '@/ui/utils/styled';
-import { openInternalPageInTab, useWallet } from 'ui/utils';
-import { Account } from '@/background/service/preference';
-import { ReactComponent as RcIconCopy } from 'ui/assets/component/icon-copy-cc.svg';
+import { useWallet } from 'ui/utils';
+import { ReactComponent as RcIconCopy } from 'ui/assets/component/copy.svg';
 import IconSuccess from 'ui/assets/success.svg';
 import { Button, message } from 'antd';
 import { copyTextToClipboard } from '@/ui/utils/clipboard';
-import { generateAliasName } from '@/utils/account';
-import { BRAND_ALIAN_TYPE_TEXT, KEYRING_CLASS, KEYRING_TYPE } from '@/constant';
-import LogoSVG from '@/ui/assets/logo.svg';
-import { ReactComponent as RcIconBack } from 'ui/assets/back-cc.svg';
+import { KEYRING_CLASS } from '@/constant';
 import { useTranslation } from 'react-i18next';
-import ThemeIcon from '@/ui/component/ThemeMode/ThemeIcon';
-
-const AlertBlock = styled.div`
-  padding: 10px 12px;
-  color: var(--r-red-default);
-  background: var(--r-red-light);
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 16px;
-`;
-
-const CopySection = styled.div`
-  color: var(--r-neutral-body);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  line-height: 1;
-
-  cursor: pointer;
-`;
-
-const TipTextList = styled.ol`
-  list-style-type: decimal;
-
-  > li {
-    font-weight: 400;
-    color: var(--r-neutral-body);
-    line-height: 20px;
-  }
-
-  > li + li {
-    margin-top: 4px;
-  }
-`;
-
-const MnemonicsWrapper = styled.div`
-  & + ${styid(TipTextList)} {
-    margin-top: 20px;
-  }
-`;
+import { Card } from '@/ui/component/NewUserImport';
+import { useHistory } from 'react-router-dom';
 
 const DisplayMnemonic = () => {
   const dispatch = useRabbyDispatch();
@@ -63,6 +18,7 @@ const DisplayMnemonic = () => {
   useEffect(() => {
     dispatch.createMnemonics.prepareMnemonicsAsync();
   }, []);
+  const history = useHistory();
   const { t } = useTranslation();
   const { mnemonics } = useRabbySelector((s) => ({
     mnemonics: s.createMnemonics.mnemonics,
@@ -78,88 +34,81 @@ const DisplayMnemonic = () => {
     });
   }, [mnemonics]);
 
-  const onSubmit = React.useCallback(() => {
-    wallet.createKeyringWithMnemonics(mnemonics).then(async () => {
-      // Passphrase is not supported on new creation
-      const keyring = await wallet.getKeyringByMnemonic(mnemonics, '');
-      const keyringId = await wallet.getMnemonicKeyRingIdFromPublicKey(
-        keyring!.publicKey!
-      );
+  const onSubmit = React.useCallback(async () => {
+    await wallet.createKeyringWithMnemonics(mnemonics);
 
-      openInternalPageInTab(
-        `import/select-address?hd=${KEYRING_CLASS.MNEMONIC}&keyringId=${keyringId}`
-      );
-
-      dispatch.createMnemonics.reset();
+    // Passphrase is not supported on new creation
+    const keyring = await wallet.getKeyringByMnemonic(mnemonics, '');
+    const keyringId = await wallet.getMnemonicKeyRingIdFromPublicKey(
+      keyring!.publicKey!
+    );
+    dispatch.importMnemonics.switchKeyring({
+      stashKeyringId: keyringId as number,
     });
+
+    const accounts = await dispatch.importMnemonics.getAccounts({
+      start: 0,
+      end: 1,
+    });
+    await dispatch.importMnemonics.setSelectedAccounts([accounts[0].address]);
+    await dispatch.importMnemonics.confirmAllImportingAccountsAsync();
+
+    history.push({
+      pathname: '/new-user/success',
+      search: `?hd=${
+        KEYRING_CLASS.MNEMONIC
+      }&keyringId=${keyringId}&isCreated=${true}`,
+    });
+    dispatch.createMnemonics.reset();
   }, [mnemonics]);
 
   return (
-    <div className="mx-auto w-[600px]">
-      <img src={LogoSVG} alt="Rabby" className="mb-[12px]" />
+    <Card onBack={() => dispatch.createMnemonics.stepTo('risk-check')} step={1}>
+      <div className="mt-[18px] mb-[9px] text-[28px] font-medium text-r-neutral-title1 text-center">
+        {t('page.newAddress.seedPhrase.backup')}
+      </div>
+      <div className="text-16 text-rabby-blue-main font-semibold text-center mb-20 mx-[30px]">
+        {t('page.newAddress.seedPhrase.backupTips')}
+      </div>
+
+      {mnemonics && (
+        <WordsMatrix
+          focusable={false}
+          closable={false}
+          words={mnemonics.split(' ')}
+          className="bg-transparent"
+        />
+      )}
+
       <div
         className={clsx(
-          'px-[100px] pt-[32px] pb-[40px]',
-          'bg-r-neutral-card-1 rounded-[12px]',
-          'relative'
+          'mx-auto mt-[24px] mb-[47px]',
+          'cursor-pointer',
+          'flex justify-center items-center gap-8',
+          'text-14 font-medium text-rabby-blue-main',
+          'hover:text-rabby-blue-default'
+        )}
+        onClick={onCopyMnemonics}
+      >
+        <RcIconCopy
+          viewBox="0 0 20 20"
+          className="w-20 h-20 text-rabby-blue-main"
+        />
+        <span>{t('page.newAddress.seedPhrase.copy')}</span>
+      </div>
+
+      <Button
+        onClick={onSubmit}
+        block
+        type="primary"
+        className={clsx(
+          'h-[56px] shadow-none rounded-[8px]',
+          'text-[17px] font-medium bg-r-blue-main'
         )}
       >
-        <div
-          className="cursor-pointer absolute left-[100px] top-[32px]"
-          onClick={() => dispatch.createMnemonics.stepTo('risk-check')}
-        >
-          <ThemeIcon
-            src={RcIconBack}
-            className="w-[20px] h-[20px] text-r-neutral-title-1"
-          />
-        </div>
-        <h1
-          className={clsx(
-            'flex items-center justify-center',
-            'space-x-[16px] mb-[14px]',
-            'text-[20px] text-r-neutral-title-1'
-          )}
-        >
-          <span>{t('page.newAddress.seedPhrase.backup')}</span>
-        </h1>
-        <div className="px-20 pt-24">
-          <AlertBlock className="flex justify-center items-center mb-[24px] rounded-[4px]">
-            <InfoCircleOutlined className="mr-10" />
-            <p className="mb-0">{t('page.newAddress.seedPhrase.backupTips')}</p>
-          </AlertBlock>
-          <MnemonicsWrapper className="relative">
-            <div className="rounded-[6px] flex items-center">
-              <WordsMatrix
-                focusable={false}
-                closable={false}
-                words={mnemonics.split(' ')}
-                className="bg-r-neutral-card-3 border-[0.5px] border-rabby-neutral-line"
-              />
-            </div>
-          </MnemonicsWrapper>
-          <CopySection
-            onClick={onCopyMnemonics}
-            className="text-13 pt-16 pb-16 mt-8"
-          >
-            <ThemeIcon
-              className="mr-6 w-[16px] h-[16px] text-r-neutral-body"
-              src={RcIconCopy}
-            />
-            {t('page.newAddress.seedPhrase.copy')}
-          </CopySection>
-        </div>
-        <div className="text-center mt-[116px]">
-          <Button
-            type="primary"
-            size="large"
-            onClick={onSubmit}
-            className="py-[13px] px-[56px] h-auto"
-          >
-            {t('page.newAddress.seedPhrase.saved')}
-          </Button>
-        </div>
-      </div>
-    </div>
+        {t('page.newAddress.seedPhrase.saved')}
+      </Button>
+    </Card>
   );
 };
 
