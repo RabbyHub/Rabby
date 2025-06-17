3b8bfda27f0204340e9acd98897fcef9d87c1f88
hz002 <zs4733525@gmail.com>
2025-06-17 14:59:35 +0800

    feat: risk tips for confirm address

---
+++ src/ui/hooks/useAddressRisk.ts
@@ -0,0 +1,174 @@
+import { AddrDescResponse } from '@rabby-wallet/rabby-api/dist/types';
+import { useEffect, useMemo, useRef, useState } from 'react';
+import PQueue from 'p-queue';
+import { useRabbyDispatch, useRabbySelector } from '../store';
+import { isValidAddress } from '@ethereumjs/util';
+import { isSameAddress, useWallet } from '../utils';
+import { KEYRING_CLASS } from 'consts';
+
+const queue = new PQueue({ intervalCap: 5, concurrency: 5, interval: 1000 });
+
+const waitQueueFinished = (q: PQueue) => {
+  return new Promise((resolve) => {
+    q.on('empty', () => {
+      if (q.pending <= 0) {
+        resolve(null);
+      }
+    });
+  });
+};
+
+export const enum RiskType {
+  NEVER_SEND = 1,
+  SCAM_ADDRESS = 2,
+  CONTRACT_ADDRESS = 3,
+  CEX_NO_DEPOSIT = 4,
+}
+
+export const useAddressRisks = (address: string) => {
+  const [addressDesc, setAddressDesc] = useState<
+    AddrDescResponse['desc'] | undefined
+  >();
+  const [loadingAddrDesc, setLoadingAddrDesc] = useState(true);
+  const [hasNoSend, setHasNoSend] = useState(false);
+  const [loadingHasTransfer, setLoadingHasTransfer] = useState(true);
+
+  const risks = useMemo(() => {
+    const isContract = Object.keys(addressDesc?.contract || {}).length > 0;
+    const isSafeAddress = Object.keys(addressDesc?.contract || {}).some(
+      (key) => {
+        const contract = addressDesc?.contract?.[key];
+        return !!contract?.multisig;
+      }
+    );
+    return [
+      addressDesc?.cex?.id && !addressDesc.cex.is_deposit
+        ? { type: RiskType.CEX_NO_DEPOSIT, value: 'CEX_NO_DEPOSIT' }
+        : null,
+      addressDesc?.is_danger || addressDesc?.is_scam
+        ? { type: RiskType.SCAM_ADDRESS, value: 'SCAM_ADDRESS' }
+        : null,
+      isContract && !isSafeAddress
+        ? { type: RiskType.CONTRACT_ADDRESS, value: 'CONTRACT_ADDRESS' }
+        : null,
+      hasNoSend ? { type: RiskType.NEVER_SEND, value: 'NEVER_SEND' } : null,
+    ].filter((i) => !!i);
+  }, [addressDesc, hasNoSend]);
+  const dispatch = useRabbyDispatch();
+  const wallet = useWallet();
+  const { accountsList } = useRabbySelector((s) => ({
+    accountsList: s.accountToDisplay.accountsList,
+  }));
+  const myTop10AccountList = useMemo(
+    () =>
+      accountsList
+        .filter(
+          (acc) =>
+            acc.type !== KEYRING_CLASS.WATCH &&
+            acc.type !== KEYRING_CLASS.GNOSIS
+        )
+        .sort((a, b) => a.balance - b.balance)
+        .slice(0, 10),
+    [accountsList]
+  );
+  useEffect(() => {
+    if (!isValidAddress(address)) {
+      return;
+    }
+    dispatch.accountToDisplay.getAllAccountsToDisplay();
+  }, []);
+
+  const riskGetRef = useRef(false);
+
+  useEffect(() => {
+    if (!isValidAddress(address)) {
+      return;
+    }
+    setLoadingAddrDesc(true);
+    wallet.openapi
+      .addrDesc(address)
+      .then((res) => {
+        if (res) {
+          setAddressDesc(res.desc);
+        }
+      })
+      .finally(() => {
+        setLoadingAddrDesc(false);
+      });
+  }, [address, dispatch]);
+
+  useEffect(() => {
+    if (
+      riskGetRef.current ||
+      !myTop10AccountList.length ||
+      !isValidAddress(address)
+    ) {
+      return;
+    }
+    riskGetRef.current = true;
+    (async () => {
+      setLoadingHasTransfer(true);
+      let hasSended = false;
+      let hasError = false;
+      try {
+        const timeoutPromise = new Promise((_, reject) => {
+          setTimeout(() => reject(new Error('timeout')), 2000);
+        });
+        const checkTransferPromise = new Promise<void>((resolve) => {
+          myTop10AccountList.forEach((acc) => {
+            if (isSameAddress(acc.address, address)) {
+              return;
+            }
+            queue.add(async () => {
+              try {
+                if (hasSended || hasError) {
+                  return;
+                }
+                console.log(
+                  'CUSTOM_LOGGER:=>: hasTransferAllChain',
+                  acc.address.slice(-4),
+                  address.slice(-4)
+                );
+                const res = await wallet.openapi.hasTransferAllChain(
+                  acc.address,
+                  address
+                );
+                if (res?.has_transfer) {
+                  hasSended = true;
+                  queue.clear();
+                  resolve();
+                }
+              } catch (error) {
+                console.error('has_transfer fetch error', error);
+                hasError = true;
+                queue.clear();
+                resolve();
+              }
+            });
+          });
+          waitQueueFinished(queue).then(() => resolve());
+        });
+
+        await Promise.race([checkTransferPromise, timeoutPromise]);
+        console.log('CUSTOM_LOGGER:=>: hasSended', hasSended, hasError);
+        if (!hasSended && !hasError) {
+          setHasNoSend(true);
+        }
+      } catch (error) {
+        console.error('check transfer timeout or error', error);
+        queue.clear();
+      } finally {
+        setLoadingHasTransfer(false);
+      }
+    })();
+  }, [address, myTop10AccountList, wallet]);
+
+  return {
+    risks,
+    addressDesc,
+    hasNoSend,
+    loading: loadingHasTransfer || loadingAddrDesc,
+    loadingAddrDesc,
+    loadingHasTransfer,
+  };
+};
