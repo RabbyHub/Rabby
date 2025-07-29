0dfa67b7e70220094e88ee15ac71c7fdbdf3ee83
DMY <147dmy@gmail.com>
2025-07-29 16:55:02 +0800

    fix: isEnabledDappAccount account

--- src/background/controller/provider/index.ts
+++ src/background/controller/provider/index.ts
@@ -34,9 +34,18 @@ export default async <T = void>(req: ProviderRequest): Promise<T> => {
           req.account || preferenceService.getCurrentAccount() || undefined;
       } else {
         const site = permissionService.getConnectedSite(origin);
+
+        const isSpeedUpOrCancel =
+          method === 'eth_sendTransaction' &&
+          (req.data?.params?.isSpeedUp || req.data?.params?.isCancel);
+
         if (site?.isConnected) {
           account =
-            site.account || preferenceService.getCurrentAccount() || undefined;
+            (isSpeedUpOrCancel
+              ? preferenceService.getCurrentAccount()
+              : site.account) ||
+            preferenceService.getCurrentAccount() ||
+            undefined;
         }
       }
     }
