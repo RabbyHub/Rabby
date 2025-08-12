5b92d56380dfd4f97c5a5b41aab14547e224a118
DMY <147dmy@gmail.com>
2025-08-12 13:15:59 +0800

    feat: Add support for OneKey and Trezor hardware wallets
    - Updated AddAddressOptions to route to new OneKey and Trezor connect pages.
    - Created OneKeyConnect and TrezorConnect components with respective banners and styles.
    - Added routing for OneKey and Trezor connections in MainRoute.
    - Updated usePreference hook to include new hardware connection paths.

---
+++ src/ui/views/ImportHardware/OneKeyConnect/Bannder.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ReactComponent as LedgerBannerArrowSVG } from '@/ui/assets/ledger/button-arrow-cc.svg';
+import clsx from 'clsx';
+
+interface Props {
+  className?: string;
+}
+export const OneKeyBanner: React.FC<Props> = ({ className }) => {
+  return (
+    <div className={className}>
+      <div
+        className={clsx(
+          'bg-r-neutral-card-2 rounded-[12px]',
+          'w-[800px] h-[100px] px-[50px]',
+          'flex items-center justify-between'
+        )}
+      ></div>
+    </div>
+  );
+};
