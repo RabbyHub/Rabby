c98f4ab18e991d9805fd64fc80a4ebb7032726f6
kim12322222 <jinming0228@qq.com>
2025-09-09 11:04:55 +0800

    fix: add pnl negative in single coin

--- src/ui/views/Perps/components/SingleCoin.tsx
+++ src/ui/views/Perps/components/SingleCoin.tsx
@@ -388,7 +388,7 @@ export const PerpsSingleCoin = () => {
                       : 'text-r-red-default'
                   )}
                 >
-                  {positionData && positionData.pnl >= 0 ? '+' : ''}$
+                  {positionData && positionData.pnl >= 0 ? '+' : '-'}$
                   {Math.abs(positionData?.pnl || 0).toFixed(2)} (
                   {positionData && positionData.pnl >= 0 ? '+' : ''}
                   {positionData?.pnlPercent.toFixed(2)}%)
