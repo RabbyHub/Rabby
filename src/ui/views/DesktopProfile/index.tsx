c33f136c0255d33de8b44387439356e720b39323
Hodor <lost1q84@gmail.com>
2025-11-21 11:15:11 +0800

    fix: scroll to top

--- src/ui/views/DesktopProfile/index.tsx
+++ src/ui/views/DesktopProfile/index.tsx
@@ -272,6 +272,18 @@ export const DesktopProfile = () => {
                 </div>
               </div>
               <ReachedEnd />
+              <div className="flex justify-end px-[20px]">
+                <BackTop
+                  target={() => scrollContainerRef.current || window}
+                  style={{
+                    bottom: 32,
+                    zIndex: 100,
+                    right: 'initial',
+                  }}
+                >
+                  <ThemeIcon src={RcIconBackTop} />
+                </BackTop>
+              </div>
             </main>
             <aside
               className={clsx(
@@ -281,16 +293,6 @@ export const DesktopProfile = () => {
               <DesktopSelectAccountList />
             </aside>
           </div>
-          <BackTop
-            target={() => scrollContainerRef.current || window}
-            style={{
-              left: '50%',
-              bottom: 32,
-              transform: 'translateX(700px)',
-            }}
-          >
-            <ThemeIcon src={RcIconBackTop} />
-          </BackTop>
         </div>
       </Wrap>
       <SendTokenModal
