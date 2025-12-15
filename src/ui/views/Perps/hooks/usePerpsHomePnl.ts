25d447b6874eeb1a97b365aacc9257be509800d1
Hodor <lost1q84@gmail.com>
2025-12-15 16:08:14 +0800

    feat: update home dashboard perps

--- src/ui/views/Perps/hooks/usePerpsHomePnl.ts
+++ src/ui/views/Perps/hooks/usePerpsHomePnl.ts
@@ -7,28 +7,16 @@ export const usePerpsHomePnl = () => {
   const perpsState = useRabbySelector((state) => state.perps);
   const perpsAccount = perpsState.currentPerpsAccount;
 
-  console.log('perps', perpsState);
-
-  const { loading: isFetching } = useRequest(
+  const { data, loading: isFetching } = useRequest(
     async () => {
       const sdk = getPerpsSDK();
       const account = perpsAccount;
       if (account?.address) {
         const res = await sdk.info.getClearingHouseState(account.address);
 
-        const positionAndOpenOrders = res.assetPositions;
-
-        if (!positionAndOpenOrders || positionAndOpenOrders.length === 0) {
-          dispatch.perps.setHomePositionPnl({ pnl: 0, show: false });
-        } else {
-          const pnl = positionAndOpenOrders.reduce((acc, asset) => {
-            return acc + Number(asset.position.unrealizedPnl);
-          }, 0);
-          dispatch.perps.setHomePositionPnl({ pnl, show: true });
-        }
-      } else {
-        dispatch.perps.setHomePositionPnl({ pnl: 0, show: false });
+        return res;
       }
+      return null;
     },
     {
       refreshDeps: [perpsAccount?.address],
@@ -36,7 +24,7 @@ export const usePerpsHomePnl = () => {
   );
 
   return {
-    perpsPositionInfo: perpsState.homePositionPnl,
+    perpsPositionInfo: data,
     isFetching,
   };
 };
