4308872b6343641d1f3242ce1b43df6bde45c840
hz002 <zs4733525@gmail.com>
2025-12-23 12:11:15 +0800

    feat: lp token manage v2

--- src/ui/views/DesktopProfile/components/TokensTabPane/components/LpTokenTag.tsx
+++ src/ui/views/DesktopProfile/components/TokensTabPane/components/LpTokenTag.tsx
@@ -6,18 +6,17 @@ import { useTranslation } from 'react-i18next';
 
 import { ReactComponent as RCLpTokenIconCC } from '@/ui/assets/lpToken-cc.svg';
 
-const Overlay = () => {
+const Overlay = ({ protocolName }: { protocolName: string }) => {
   const { t } = useTranslation();
   return (
     <div className="flex flex-col gap-8 py-6">
       <div className="flex items-center gap-4">
         <RCLpTokenIconCC className="w-16 h-16 text-r-neutral-title2 cursor-pointer" />
         <div className="text-12 text-white font-semibold">
-          {t('component.LpTokenTag.title')}
+          {t('component.LpTokenTag.descriptionFor', {
+            protocolName,
+          })}
         </div>
-      </div>
-      <div className="text-12 text-rb-neutral-info">
-        {t('component.LpTokenTag.description')}
       </div>
     </div>
   );
@@ -27,21 +26,23 @@ export const LpTokenTag = ({
   iconClassName,
   className,
   size = 16,
+  protocolName,
 }: {
   iconClassName?: string;
   className?: string;
   size?: number;
+  protocolName: string;
 }) => {
   return (
     <Tooltip
       overlayClassName={cx('rectangle addressType__tooltip')}
-      overlayStyle={{ maxWidth: '280px' }}
+      overlayStyle={{ maxWidth: '320px' }}
       overlayInnerStyle={{
         borderRadius: '12px',
       }}
       className={className}
       style={{ width: size, height: size }}
-      overlay={<Overlay />}
+      overlay={<Overlay protocolName={protocolName} />}
       mouseEnterDelay={0}
     >
       <RCLpTokenIconCC
