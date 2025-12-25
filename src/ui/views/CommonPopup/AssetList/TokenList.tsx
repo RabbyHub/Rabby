1f7afaafc5f7086820fb0307c3b54274d4559b4e
hz002 <zs4733525@gmail.com>
2025-12-25 17:50:53 +0800

    feat: lp token manage v2 (#3374)
    * feat: lp token manage v2
    
    * fix: bugs
    
    * fix: bugs
    
    * fix: bugs

--- src/ui/views/CommonPopup/AssetList/TokenList.tsx
+++ src/ui/views/CommonPopup/AssetList/TokenList.tsx
@@ -19,6 +19,7 @@ export interface Props {
   customizeTokens?: TokenItemProps['item'][];
   isTestnet: boolean;
   selectChainId?: string | null;
+  lpTokenMode?: boolean;
 }
 
 export const HomeTokenList = ({
@@ -31,6 +32,7 @@ export const HomeTokenList = ({
   customizeTokens,
   isTestnet,
   selectChainId,
+  lpTokenMode,
 }) => {
   const totalValue = React.useMemo(() => {
     return list
@@ -60,6 +62,20 @@ export const HomeTokenList = ({
         className="mt-[92px]"
         text={t('page.dashboard.assets.table.noMatch')}
       />
+    );
+  }
+  if (lpTokenMode && !list?.length) {
+    return (
+      <div className="no-token w-full flex flex-col items-center justify-center h-[154px] bg-r-neutral-card1 rounded-[8px]">
+        <img
+          className="w-[72px] h-[72px]"
+          src="/images/nodata-tx.png"
+          alt="no site"
+        />
+        <p className="text-r-neutral-foot text-13 mt-2 text-center mb-0">
+          {t('component.TokenSelector.noLpTokens')}
+        </p>
+      </div>
     );
   }
 
