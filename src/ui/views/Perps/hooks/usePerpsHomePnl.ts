3b122413e0fb3a2ca9430256d79698d4ad37c3ef
kim <180150277+jinming0618@users.noreply.github.com>
2026-01-16 15:45:06 +0800

    feat: update home pnl (#3413)

--- src/ui/views/Perps/hooks/usePerpsHomePnl.ts
+++ src/ui/views/Perps/hooks/usePerpsHomePnl.ts
@@ -1,5 +1,6 @@
 import { useRabbySelector } from '@/ui/store';
 import { usePerpsClearHouseState } from './usePerpsClearingHouseState';
+import { useMemo } from 'react';
 
 export const usePerpsHomePnl = () => {
   const perpsState = useRabbySelector((state) => state.perps);
@@ -9,8 +10,15 @@ export const usePerpsHomePnl = () => {
     address: perpsAccount?.address,
   });
 
+  const pnl = useMemo(() => {
+    return data?.assetPositions.reduce((acc, item) => {
+      return acc + Number(item.position.unrealizedPnl);
+    }, 0);
+  }, [data]);
+
   return {
     perpsPositionInfo: data,
     isFetching,
+    positionPnl: pnl,
   };
 };
