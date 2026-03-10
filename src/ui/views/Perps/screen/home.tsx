1f331090b6315acb0e43a161d2fa497053a4c94e
Hodor <lost1q84@gmail.com>
2026-03-10 18:10:36 +0800

    fix: style

--- src/ui/views/Perps/screen/home.tsx
+++ src/ui/views/Perps/screen/home.tsx
@@ -393,9 +393,6 @@ export const Perps: React.FC = () => {
       maskClosable: true,
       centered: true,
       title: null,
-      bodyStyle: {
-        padding: 0,
-      },
       className: clsx(
         'perps-bridge-swap-modal perps-close-all-position-modal',
         isDarkTheme
