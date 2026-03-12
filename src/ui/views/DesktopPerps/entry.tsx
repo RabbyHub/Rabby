b4da6ce2b5584ac7caeb64cb91e6a72a5f24b66a
Hodor <lost1q84@gmail.com>
2026-03-12 18:15:44 +0800

    fix: perps icon

--- src/ui/views/DesktopPerps/entry.tsx
+++ src/ui/views/DesktopPerps/entry.tsx
@@ -1,7 +1,5 @@
-import { useRabbySelector } from '@/ui/store';
 import { DesktopPerps } from '.';
 import React from 'react';
-import { DesktopInnerDapp } from '../DesktopDappIframe';
 
 export const DesktopPerpsEntry = ({ isActive }: { isActive?: boolean }) => {
   return <DesktopPerps isActive={isActive} />;
