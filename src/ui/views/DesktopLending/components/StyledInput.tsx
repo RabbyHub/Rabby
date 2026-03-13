3758353a81420a197e7ac66c8a29e79bacfde809
hz002 <zs4733525@gmail.com>
2026-03-13 19:43:32 +0800

    feat: lending swap (#3543)
    * feat: repay with collateral
    
    * feat: debt swap
    
    * fix: ui
    
    * fix: custom gas popup
    
    * fix: maxNewDebtAmount
    
    * fix: ui
    
    * fix: show price impact

--- src/ui/views/DesktopLending/components/StyledInput.tsx
+++ src/ui/views/DesktopLending/components/StyledInput.tsx
@@ -3,10 +3,10 @@ import styled from 'styled-components';
 import { Input, InputProps } from 'antd';
 import { formatSpeicalAmount } from '../utils/format';
 
-const StyledInputComponent = styled(Input)`
+const StyledInputComponent = styled(Input)<{ $fontSize: string }>`
   border-right-width: 0 !important;
   border-color: transparent !important;
-  font-size: 20px !important;
+  font-size: ${({ $fontSize }) => $fontSize} !important;
   line-height: 28px !important;
   font-weight: 500 !important;
   color: var(--r-neutral-title-1) !important;
@@ -14,15 +14,19 @@ const StyledInputComponent = styled(Input)`
 
 interface StyledInputProps extends InputProps {
   onValueChange?: (v: string) => void;
+  fontSize?: number | string;
 }
 export const LendingStyledInput = (props: StyledInputProps) => {
+  const { fontSize = 20, onValueChange, ...rest } = props;
+  const resolvedFontSize =
+    typeof fontSize === 'number' ? `${fontSize}px` : fontSize;
+
   return (
     <StyledInputComponent
-      {...props}
+      {...rest}
+      $fontSize={resolvedFontSize}
       autoFocus
-      onChange={(e) =>
-        props.onValueChange?.(formatSpeicalAmount(e.target.value))
-      }
+      onChange={(e) => onValueChange?.(formatSpeicalAmount(e.target.value))}
     />
   );
 };
