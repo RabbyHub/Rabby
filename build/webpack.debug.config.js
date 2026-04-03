1f7f2ca088bd1c97999a55141130d07409f1b26d
ru1n <chengrui191954@gmail.com>
2026-04-03 15:32:40 +0800

    sec: disable

--- build/webpack.debug.config.js
+++ build/webpack.debug.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 const SecSDK = require('warden-for-js').WardenPlugin;
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
@@ -16,7 +17,7 @@ const config = {
       'process.env.BUILD_ENV': JSON.stringify('PRO'),
       'process.env.DEBUG': true,
     }),
-    true &&
+    false &&
       new SecSDK({
         dev: false,
         disableProtoAssets: ['pageProvider.js'],
@@ -46,6 +47,19 @@ const config = {
         ],
       }),
   ].filter(Boolean),
+
+  optimization: {
+    minimize: false,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            pure_funcs: ['console.log', 'console.debug', 'console.info'],
+          },
+        },
+      }),
+    ],
+  },
 };
 
 module.exports = config;
