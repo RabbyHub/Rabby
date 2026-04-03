ee546f96dd3fcf467ae5c0e9dfdcd0748175da16
Hodor <lost1q84@gmail.com>
2026-04-03 19:40:13 +0800

    fix: gas selector popup close (#3616)
    * fix: gas selector popup close
    
    * fix: gas withdraw height

--- src/ui/views/Bridge/Component/ShowMoreGasModal.tsx
+++ src/ui/views/Bridge/Component/ShowMoreGasModal.tsx
@@ -139,6 +139,9 @@ export default function ShowMoreGasSelectModal({
     <Dropdown
       onOpenChange={(v) => {
         setVisible(v);
+        if (!v) {
+          onCancel();
+        }
       }}
       open={visible}
       placement="topRight"
