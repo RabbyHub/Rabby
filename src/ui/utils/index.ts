a752e1fcfa5500f0f49ab9558bebca9c1cf5f62b
ru1n <113791864+ddddhm1234@users.noreply.github.com>
2026-04-17 19:04:07 +0800

    sec: fix 2 vulnerability (#3654)
    * sec: fix personal_sign hex2Text
    
    * sec: normalize NativeMetaTransaction typedData before parseCommon & parseAction

--- src/ui/utils/index.ts
+++ src/ui/utils/index.ts
@@ -61,7 +61,7 @@ export const hex2Text = (hex: string) => {
   try {
     return hex.startsWith('0x')
       ? decodeURIComponent(
-          hex.replace(/^0x/, '').replace(/[0-9a-f]{2}/g, '%$&')
+          hex.replace(/^0x/, '').replace(/[0-9a-fA-F]{2}/g, '%$&')
         )
       : hex;
   } catch {
