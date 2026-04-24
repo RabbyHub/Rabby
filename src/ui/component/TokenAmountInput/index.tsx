d384d12ae42a21191d5b26a97adbe458b87c40af
Hodor <lost1q84@gmail.com>
2026-04-24 10:39:56 +0800

    fix: desktop send token select chain

--- src/ui/component/TokenAmountInput/index.tsx
+++ src/ui/component/TokenAmountInput/index.tsx
@@ -475,6 +475,7 @@ const TokenAmountInput = ({
         onChange={(value) => {
           setChainServerId(findChainByEnum(value)?.serverId || '');
         }}
+        getContainer={getContainer}
       />
     </div>
   );
