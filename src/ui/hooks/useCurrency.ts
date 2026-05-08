fd76758b335cda7c04425043e755863dfa99bf55
richardo2016x <richardo2016x@gmail.com>
2026-05-08 16:22:38 +0800

    fix: align default currency type

--- src/ui/hooks/useCurrency.ts
+++ src/ui/hooks/useCurrency.ts
@@ -7,7 +7,6 @@ const USD_CURRENCY: CurrencyItem = {
   code: 'USD',
   symbol: '$',
   usd_rate: 1,
-  is_prefix: true,
   logo_url:
     'https://static.debank.com/image/country/logo_url/f253efe302d32ab264a76e0ce65be769/d47bf4d88f1d19912103106e80e4722c.png',
 };
