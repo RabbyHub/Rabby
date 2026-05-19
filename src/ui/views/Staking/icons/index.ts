fad7680c8a0db34af5699c85c69b4171b1ce7580
guangmingpeng <2998583934@qq.com>
2026-05-19 15:51:18 +0800

    refactor(staking): move svg icons into assets

---
+++ src/ui/views/Staking/icons/index.ts
@@ -0,0 +1,7 @@
+export { ReactComponent as BackIcon } from './back.svg';
+export { ReactComponent as KeyIcon } from './key.svg';
+export { ReactComponent as DownIcon } from './down.svg';
+export { ReactComponent as SearchIcon } from './search.svg';
+export { ReactComponent as CloseIcon } from './close.svg';
+export { ReactComponent as ExternalIcon } from './external.svg';
+export { ReactComponent as EmptyPoolsIcon } from './empty-pools.svg';
