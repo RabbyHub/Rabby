d2560c4e1551220cb4eff6750960338c36f7e61d
Hodor <lost1q84@gmail.com>
2026-06-15 11:30:42 +0800

    fix: tailwind

--- src/ui/views/DesktopPerps/components/OrderBookTrades/components/OrderBook.tsx
+++ src/ui/views/DesktopPerps/components/OrderBookTrades/components/OrderBook.tsx
@@ -381,7 +381,7 @@ export const OrderBook: React.FC<{ latestTrade?: Trade }> = ({
         <div
           className={clsx(
             'absolute top-0 bottom-0 transition-[width] duration-200 ease-out',
-            type === 'bid' ? 'left-0' : 'right-0',
+            'right-0',
             type === 'bid' ? 'bg-rb-green-light-1' : 'bg-rb-red-light-1'
           )}
           style={{ width: `${depthPercent}%` }}
