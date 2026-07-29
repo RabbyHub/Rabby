13f602a5078e3e45b632062e5ec68eb03524c217
hz002 <zs4733525@gmail.com>
2026-07-29 18:03:18 +0800

    fix: eslint

--- src/ui/views/Perps/popup/OpenPositionPopup.tsx
+++ src/ui/views/Perps/popup/OpenPositionPopup.tsx
@@ -837,14 +837,14 @@ export const PerpsOpenPositionPopup: React.FC<OpenPositionPopupProps> = ({
           slippageReady &&
           Number(tradeSize) > 0 &&
           slippage > PERPS_SLIPPAGE_DISPLAY_MIN && (
-          <div className="bg-r-neutral-card1 rounded-[8px] py-12 px-16 mb-12">
-            <MarketSlippage
-              slippage={slippage}
-              depthInsufficient={depthInsufficient}
-              onSwitchToLimit={handleSwitchToLimit}
-            />
-          </div>
-        )}
+            <div className="bg-r-neutral-card1 rounded-[8px] py-12 px-16 mb-12">
+              <MarketSlippage
+                slippage={slippage}
+                depthInsufficient={depthInsufficient}
+                onSwitchToLimit={handleSwitchToLimit}
+              />
+            </div>
+          )}
 
         {/* Action Buttons */}
         <div className="fixed bottom-0 left-0 right-0">
