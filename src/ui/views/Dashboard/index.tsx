72dfc64d5f344fe75b9169b5e9d63b042e7d01ae
hz002 <zs4733525@gmail.com>
2026-08-27 14:01:00 +0800

    feat: bottom ad popup (#4015)
    * feat: bottom ad popup
    
    * feat: hide logic
    
    * docs: button text
    
    * feat: change both side swap token
    
    * fix: scroll
    
    * style: fix
    
    * feat: report ad action

--- src/ui/views/Dashboard/index.tsx
+++ src/ui/views/Dashboard/index.tsx
@@ -1,4 +1,3 @@
-import clsx from 'clsx';
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import ReactMarkdown from 'react-markdown';
@@ -25,6 +24,7 @@ import {
   useGasAccountDiscovery,
   useGasAccountSign,
 } from '@/ui/views/GasAccount/hooks';
+import { StablecoinSwapPopup } from './components/StablecoinSwapPopup';
 import { useAppVersionStore } from '@/ui/state/appVersion';
 
 const Dashboard = () => {
@@ -189,13 +189,18 @@ const Dashboard = () => {
 
   return (
     <>
-      <div className={clsx('dashboard')}>
-        <DashboardHeader onSettingClick={toggleShowMoreSettings} />
-        <DashboardPanel onSettingClick={toggleShowMoreSettings} />
-        <div className="px-[16px] pb-[13px]">
-          <GasPriceBar currentConnectedSiteChain={currentConnectedSiteChain} />
-          <CurrentConnection onChainChange={setCurrentConnectedSiteChain} />
+      <div className="relative h-full overflow-hidden">
+        <div className="dashboard h-full overflow-y-auto">
+          <DashboardHeader onSettingClick={toggleShowMoreSettings} />
+          <DashboardPanel onSettingClick={toggleShowMoreSettings} />
+          <div className="px-[16px] pb-[13px]">
+            <GasPriceBar
+              currentConnectedSiteChain={currentConnectedSiteChain}
+            />
+            <CurrentConnection onChainChange={setCurrentConnectedSiteChain} />
+          </div>
         </div>
+        <StablecoinSwapPopup />
       </div>
       <Modal
         visible={firstNotice && updateContent}
