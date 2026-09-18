77d9e9efb78c17c53154abe1317f728fc12d02eb
hz002 <zs4733525@gmail.com>
2026-09-18 14:58:56 +0800

    feat: check swap receiver (#4102)
    * feat: check swap receiver
    
    * feat: update rabby-swap version
    
    * fix: cleanup

--- src/ui/views/Swap/hooks/index.tsx
+++ src/ui/views/Swap/hooks/index.tsx
@@ -2,7 +2,6 @@ export * from './swapReport';
 export * from './token';
 export * from './settings';
 export * from './context';
-export * from './verify';
 export * from './quote';
 export * from './history';
 export * from './fee';
